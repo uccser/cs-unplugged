8064be72de340fca963da2cade2b73aa969fbdbd
Jack Morgan <jackmorgannz@gmail.com>
2017-01-29 15:33:44 +1300

    Add string representation for Activity model

--- csunplugged/activities/models.py
+++ csunplugged/activities/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Activity(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
