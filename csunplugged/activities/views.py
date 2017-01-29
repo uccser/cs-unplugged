4bd3f68b28434044428166147a037e8abdd0a44f
Jack Morgan <jackmorgannz@gmail.com>
2017-01-29 20:07:37 +1300

    Add homepage for activities

--- csunplugged/activities/views.py
+++ csunplugged/activities/views.py
@@ -1,12 +1,13 @@
-# from django.shortcuts import render
-#
-# # Create your views here.
-
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Activity
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the activities index.")
+    all_activities = Activity.objects.order_by('name')
+    context = {
+        'all_activities': all_activities
+    }
+    return render(request, 'activities/index.html', context)
 
 def description(request, activity_name):
     return HttpResponse("This is the description page for {}".format(activity_name))
