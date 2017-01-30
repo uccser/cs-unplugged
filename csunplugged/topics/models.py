94e8a2ba8e78d478dcd640a6fc11751118f58649
Jack Morgan <jackmorgannz@gmail.com>
2017-01-30 14:23:23 +1300

    Set topic slug to be unique

--- csunplugged/topics/models.py
+++ csunplugged/topics/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Topic(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
