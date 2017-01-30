5c030e00858edb30689da78422d3875c7f7e1e8e
Jack Morgan <jackmorgannz@gmail.com>
2017-01-30 14:05:06 +1300

    Add admin interface

--- csunplugged/config/urls.py
+++ csunplugged/config/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^topics/', include('topics.urls'))
+    url(r'^topics/', include('topics.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
 # ] + static(settings.STATIC_URL, documnet_root=settings.STATIC_ROOT)
--- csunplugged/topics/admin.py
+++ csunplugged/topics/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Topic
+
+admin.site.register(Topic)
