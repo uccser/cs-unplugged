2ece53c56cbabdf80bcb45d7b80a2ba487422e8b
Jack Morgan <jackmorgannz@gmail.com>
2017-02-23 15:15:43 +1300

    Demonstrate configuring resource output from form

--- csunplugged/resources/content/example_resource/generate.py
+++ csunplugged/resources/content/example_resource/generate.py
@@ -5,7 +5,7 @@ from weasyprint import HTML, CSS
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 import base64
-from random import shuffle
+from random import sample
 
 def pdf(request, resource, **kwargs):
     context = dict()
@@ -14,26 +14,43 @@ def pdf(request, resource, **kwargs):
     base_image = Image.open('resources/content/example_resource/sorting-network-colour.png')
 
     # Create numbers to add to image
-    numbers = list(range(1, 7))
-    shuffle(numbers)
+    difficulty = request.GET['difficulty']
+    if difficulty == 'easy':
+        range_min = 1
+        range_max = 10
+        font_size = 150
+    elif difficulty == 'medium':
+        range_min = 10
+        range_max = 100
+        font_size = 120
+    else:
+        range_min = 100
+        range_max = 1000
+        font_size = 90
+
+    numbers = sample(range(range_min, range_max), 6)
 
     # Load image for adding text
     draw = ImageDraw.Draw(base_image)
     font_path = 'resources/content/fonts/PatrickHand-Regular.ttf'
-    font = ImageFont.truetype(font_path, 150)
+    font = ImageFont.truetype(font_path, font_size)
 
     # Add numbers to text
-    coord_x = 45
-    coord_y = 2475
+    base_coord_x = 70
+    base_coord_y = 2560
+    coord_x_increment = 204
     for number in numbers:
+        text = str(number)
+        text_width, text_height = draw.textsize(text, font=font)
+        coord_x = base_coord_x - (text_width / 2)
+        coord_y = base_coord_y - (text_height / 2)
         draw.text(
             (coord_x, coord_y),
-            str(number),
+            text,
             font=font,
-            fill='#000',
-            align='center'
+            fill='#000'
         )
-        coord_x += 204
+        base_coord_x += coord_x_increment
 
     # Save image to buffer
     image_buffer = BytesIO()
