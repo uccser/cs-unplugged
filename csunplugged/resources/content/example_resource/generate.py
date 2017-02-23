703ac060e9f1f70364630b4cb632ade7d669bb17
Jack Morgan <jackmorgannz@gmail.com>
2017-02-23 16:28:43 +1300

    Thread generation of image of example-resource

--- csunplugged/resources/content/example_resource/generate.py
+++ csunplugged/resources/content/example_resource/generate.py
@@ -6,10 +6,51 @@ from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 import base64
 from random import sample
+from multiprocessing import Pool
+
+def generate_image(range_min, range_max, base_image_path, font):
+    numbers = sample(range(range_min, range_max), 6)
+
+    # Load image for adding text
+    base_image = Image.open(base_image_path)
+    draw = ImageDraw.Draw(base_image)
+
+    # Add numbers to text
+    base_coord_x = 70
+    base_coord_y = 2560
+    coord_x_increment = 204
+    for number in numbers:
+        text = str(number)
+        text_width, text_height = draw.textsize(text, font=font)
+        coord_x = base_coord_x - (text_width / 2)
+        coord_y = base_coord_y - (text_height / 2)
+        draw.text(
+            (coord_x, coord_y),
+            text,
+            font=font,
+            fill='#000'
+        )
+        base_coord_x += coord_x_increment
+
+    # Save image to buffer
+    image_buffer = BytesIO()
+    base_image.save(image_buffer, format='PNG')
+    return base64.b64encode(image_buffer.getvalue())
+
+class PageCreator(object):
+    def __init__(self, range_min, range_max, base_image_path, font_path, font_size):
+        self.range_min = range_min
+        self.range_max = range_max
+        self.base_image_path = base_image_path
+        self.font_path = font_path
+        self.font_size = font_size
+
+    def __call__(self, call_num):
+        font = ImageFont.truetype(self.font_path, self.font_size)
+        return generate_image(self.range_min, self.range_max, self.base_image_path, font)
 
 def pdf(request, resource, **kwargs):
     context = dict()
-    context['resource_images'] = []
 
     # Create numbers to add to image
     difficulty = request.GET['difficulty']
@@ -29,36 +70,9 @@ def pdf(request, resource, **kwargs):
     # Create resource image
     base_image_path = 'resources/content/example_resource/sorting-network-colour.png'
     font_path = 'resources/content/fonts/PatrickHand-Regular.ttf'
-    font = ImageFont.truetype(font_path, font_size)
 
-    for page_num in range(0, int(request.GET['copies'])):
-        numbers = sample(range(range_min, range_max), 6)
-
-        # Load image for adding text
-        base_image = Image.open(base_image_path)
-        draw = ImageDraw.Draw(base_image)
-
-        # Add numbers to text
-        base_coord_x = 70
-        base_coord_y = 2560
-        coord_x_increment = 204
-        for number in numbers:
-            text = str(number)
-            text_width, text_height = draw.textsize(text, font=font)
-            coord_x = base_coord_x - (text_width / 2)
-            coord_y = base_coord_y - (text_height / 2)
-            draw.text(
-                (coord_x, coord_y),
-                text,
-                font=font,
-                fill='#000'
-            )
-            base_coord_x += coord_x_increment
-
-        # Save image to buffer
-        image_buffer = BytesIO()
-        base_image.save(image_buffer, format='PNG')
-        context['resource_images'].append(base64.b64encode(image_buffer.getvalue()))
+    with Pool() as pool:
+        context['resource_images'] = pool.map(PageCreator(range_min, range_max, base_image_path, font_path, font_size), range(0, int(request.GET['copies'])))
 
     # Write to PDF
     context['paper_size'] = request.GET['size']
