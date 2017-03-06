b4f8921147686040182ba54783db7afcce9e0f7f
Jack Morgan <jackmorgannz@gmail.com>
2017-03-06 13:48:44 +1300

    Add missing module import

--- csunplugged/config/urls.py
+++ csunplugged/config/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
