c0d7795bb70d4e183e85905bfb945453122ce901
Hayley van Waas <hayleyavw@gmail.com>
2017-03-28 10:50:23 +1300

    Lists unitplans under each topic

--- csunplugged/dev/views.py
+++ csunplugged/dev/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-from django.shortcuts import get_object_or_404
 from django.views import generic
 
 from topics.models import (
@@ -22,3 +19,15 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return all topics"""
         return Topic.objects.order_by('name')
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        context['topics'] = Topic.objects.order_by('name')
+
+        context['unit_plans'] = {}
+        for topic in context['topics']:
+            unit_plan = UnitPlan.objects.get(topic=topic)
+            context['unit_plans'][topic.name] = unit_plan
+
+        return context
