7ecddaf2f1ae6f05f65157b2656fd53bcf628112
Hayley van Waas <hayleyavw@gmail.com>
2017-03-28 14:38:16 +1300

    Added curriculum areas and integrations

--- csunplugged/dev/views.py
+++ csunplugged/dev/views.py
@@ -2,6 +2,7 @@ from django.views import generic
 
 from topics.models import (
     Topic,
+    CurriculumArea,
     CurriculumIntegration,
     UnitPlan,
     Lesson,
@@ -23,16 +24,22 @@ class IndexView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
 
+        # Get topic, unit plan and lesson content
         context['topics'] = Topic.objects.order_by('name')
-
         context['unit_plans'] = {}
         context['lessons'] = {}
 
-        # build dictionaries for each unit plan and lesson
+        # Build dictionaries for each unit plan and lesson
         for topic in context['topics']:
             # TODO watch this fall apart as multiple unit plans are added to same topic
             unit_plan = UnitPlan.objects.get(topic=topic)
             context['unit_plans'][topic.name] = unit_plan
             context['lessons'][unit_plan.name] = unit_plan.lessons_by_age_group()
+
+        # Get curriculum area content
+        context['curriculum_areas'] = CurriculumArea.objects.all()
+
+        # Get curricukum integration content
+        context['cur_int_activites'] = CurriculumIntegration.objects.all()
 
         return context
