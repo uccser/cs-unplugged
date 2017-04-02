4d5dbd93adbfd3babfa10e2bb32214846b56aa12
Jack Morgan <jackmorgannz@gmail.com>
2017-04-02 19:19:30 +1200

    Store database values to allow shell commands to function

--- csunplugged/docker-compose-entrypoint.sh
+++ csunplugged/docker-compose-entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER
-
 function postgres_ready(){
 python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+    conn = psycopg2.connect(dbname="postgres", user="postgres", host="postgres")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
