a55d63fd1269b715e30deb5d0350f5f9552198ea
Jack Morgan <jackmorgannz@gmail.com>
2017-04-06 13:11:07 +1200

    Use django collectstatic for serving static files

--- csunplugged/config/settings/base.py
+++ csunplugged/config/settings/base.py
@@ -176,10 +176,10 @@ TEMPLATES = [
 # STATIC FILE CONFIGURATION
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+STATIC_ROOT = str(ROOT_DIR.path('staticfiles'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
