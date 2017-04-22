13e2afcca8119e00dbaa8993f262fa5c74a46e73
Jack Morgan <jackmorgannz@gmail.com>
2017-04-23 08:55:12 +1200

    Remove topic query from homepage (fixes #186)
    This removed an unnecessary query, saving 80 ms on
    my local development machine.

--- csunplugged/general/views.py
+++ csunplugged/general/views.py
@@ -4,14 +4,5 @@ from django.views.generic import TemplateView
 class GeneralIndexView(TemplateView):
     template_name = 'general/index.html'
 
-    def get_context_data(self, **kwargs):
-        # TODO: Investigate if importing model from another
-        # app is sensible/best approach.
-        from topics.models import Topic
-        context = super(GeneralIndexView, self).get_context_data(**kwargs)
-        context['total_topics'] = Topic.objects.count()
-        return context
-
-
 class GeneralAboutView(TemplateView):
     template_name = 'general/about.html'
