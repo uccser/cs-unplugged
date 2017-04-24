9bd056ba0dbff3c10843722fabbdfc020602184a
Jack Morgan <jackmorgannz@gmail.com>
2017-04-24 14:08:11 +1200

    PEP8 fix

--- csunplugged/general/views.py
+++ csunplugged/general/views.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 class GeneralIndexView(TemplateView):
     template_name = 'general/index.html'
 
+
 class GeneralAboutView(TemplateView):
     template_name = 'general/about.html'
