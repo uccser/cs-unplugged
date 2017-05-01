7c7ea645934ec179b31da9c096e8a056e419d54a
ravenmaster_001 <hayden.adam.jackson@gmail.com>
2017-05-01 15:57:27 +1200

    Setup renderengine for local development.

---
+++ render/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, make_response
+import os
+
+DEBUG = not int(os.environ['FLASK_PRODUCTION'])
+PORT = int(os.environ['PORT'])
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Flask Dockerized'
+
+@app.route('/_ah/health')
+def health_check():
+    content = ''
+    response = make_response(content, 200)
+    return response
+
+if __name__ == '__main__':
+    print(DEBUG, PORT)
+    app.run(debug=DEBUG, host='0.0.0.0', port=PORT)
