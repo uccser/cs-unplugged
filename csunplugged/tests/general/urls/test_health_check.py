02568861b778728f53fbec3a2d06875add0861de
Jack Morgan <jackmorgannz@gmail.com>
2017-05-01 17:25:50 +1200

    Test health check URL response

---
+++ csunplugged/tests/general/urls/test_health_check.py
@@ -0,0 +1,13 @@
+from tests.BaseTestWithDB import BaseTestWithDB
+from django.urls import reverse
+
+
+class HealthCheckURLTest(BaseTestWithDB):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.language = 'en'
+
+    def test_valid_health_check_request(self):
+        response = self.client.get('/_ah/health')
+        self.assertEqual(200, response.status_code)
