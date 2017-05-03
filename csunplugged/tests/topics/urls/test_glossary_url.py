c4f8ccd1b60ca9c5f8a02b21d5b3c0d1cf5ac5dc
Jack Morgan <jackmorgannz@gmail.com>
2017-05-03 18:53:20 +1200

    Add tests for glossary page

---
+++ csunplugged/tests/topics/urls/test_glossary_url.py
@@ -0,0 +1,13 @@
+from tests.BaseTestWithDB import BaseTestWithDB
+from django.urls import reverse
+
+
+class GlossaryURLTest(BaseTestWithDB):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.language = 'en'
+
+    def test_valid_glossary_url(self):
+        url = reverse('topics:glossary')
+        self.assertEqual(url, '/en/topics/glossary/')
