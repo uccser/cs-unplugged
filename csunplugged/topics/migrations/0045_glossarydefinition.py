1254e1a16ea615a129656834905487a145b55c44
Jack Morgan <jackmorgannz@gmail.com>
2017-05-04 11:31:58 +1200

    Simplify migration files

--- csunplugged/topics/migrations/0045_glossarydefinition.py
+++ csunplugged/topics/migrations/0045_glossarydefinition.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='GlossaryDefinition',
+            name='GlossaryTerm',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('slug', models.SlugField(unique=True)),
