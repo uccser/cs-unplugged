bffab8e9807a651b4b12489bc486aa4968bcd1a1
Jack Morgan <jackmorgannz@gmail.com>
2017-05-10 09:41:51 +1200

    Update templates and views for dev application

--- csunplugged/dev/urls.py
+++ csunplugged/dev/urls.py
@@ -12,19 +12,4 @@ urlpatterns = [
         views.IndexView.as_view(),
         name='index'
     ),
-    url(
-        r'^(?P<programming_exercise_slug>[-\w]+)/$',
-        views.ProgrammingExerciseView.as_view(),
-        name='programming_exercise'
-    ),
-    url(
-        r'^(?P<programming_exercise_slug>[-\w]+)/(?P<programming_language_slug>[-\w]+)-solution$',  # noqa: E501
-        views.ProgrammingExerciseLanguageSolutionView.as_view(),
-        name='programming_exercise_language_solution'
-    ),
-    url(
-        r'^integrations/(?P<integration_slug>[-\w]+)/$',
-        views.CurriculumIntegrationView.as_view(),
-        name='integration'
-    ),
 ]
