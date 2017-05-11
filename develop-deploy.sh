b678c1dee2a6b84f155d922e83afb3a284493fb8
Jack Morgan <jackmorgannz@gmail.com>
2017-05-11 18:16:26 +1200

    Run cloud sql proxy in background

--- develop-deploy.sh
+++ develop-deploy.sh
@@ -15,7 +15,7 @@ gcloud auth activate-service-account --key-file continuous-deployment-develop-cr
 wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64
 mv cloud_sql_proxy.linux.amd64 cloud_sql_proxy
 chmod +x cloud_sql_proxy
-./cloud_sql_proxy -instances="$GOOGLE_CLOUD_SQL_CONNECTION_NAME"=tcp:5433 -credential_file="./continuous-deployment-develop-credentials.json" &
+./cloud_sql_proxy -instances="$GOOGLE_CLOUD_SQL_CONNECTION_NAME"=tcp:5433 -credential_file="./continuous-deployment-develop-credentials.json" &>/dev/null &
 exit
 sudo apt-get install -y python3 python3-dev python3-pip
 python -m virtualenv --python=python3.4 /venv
