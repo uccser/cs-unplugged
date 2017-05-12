82837f2c5ca8a8051cd255b3d513a4f85bbbc3cb
Hayley van Waas <hayleyavw@gmail.com>
2017-05-12 15:58:48 +1200

    Fixed curriculum areas test, added useful command to csu

--- csunplugged/tests/topics/loaders/test_curriculum_integrations_loader.py
+++ csunplugged/tests/topics/loaders/test_curriculum_integrations_loader.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 from tests.BaseTestWithDB import BaseTestWithDB
 from tests.topics import create_topics_test_data as test_data
 
@@ -9,6 +11,7 @@ class CurriculumIntegrationsLoaderTest(BaseTestWithDB):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.load_log = Mock()
         self.BASE_PATH = "tests/topics/loaders/assets/curriculum_integrations/"
 
     def test_basic_config(self):
@@ -17,7 +20,7 @@ class CurriculumIntegrationsLoaderTest(BaseTestWithDB):
         test_data.create_test_curriculum_area('1')
         topic = test_data.create_test_topic('1')
 
-        ci_loader = CurriculumIntegrationsLoader(config_file, topic, self.BASE_PATH)
+        ci_loader = CurriculumIntegrationsLoader(self.load_log, config_file, topic, self.BASE_PATH)
         ci_loader.load()
 
         ci_objects = CurriculumIntegration.objects.all()
