94422d6407988d426d5a745c751eaa205d93c908
ravenmaster_001 <hayden.adam.jackson@gmail.com>
2017-05-18 12:04:36 +1200

    Update QueueHandler based off new Queue API.

--- render/QueueHandler.py
+++ render/QueueHandler.py
@@ -5,6 +5,9 @@ from apiclient.discovery import build, HttpError
 from base64 import b64encode, b64decode
 
 
+DISCOVERY_URL = os.getenv("API_DISCOVERY_URL", None)
+
+
 def authorize_session():
     """Authorize for taskqueue transactions.
 
@@ -23,7 +26,7 @@ def encode_dictionary(dictionary):
         The encoded string.
     """
     string = json.dumps(dictionary)
-    encoded_string = b64encode(string.encode('ascii')).decode()
+    encoded_string = b64encode(string.encode("ascii")).decode()
     return encoded_string
 
 
@@ -50,15 +53,31 @@ class QueueHandler(object):
             project_name: The project the taskqueue belongs to.
             taskqueue_name: The name of the taskqueue.
         """
-        self.task_api = build('taskqueue', 'v1beta2')
         self.project_name = project_name
         self.taskqueue_name = taskqueue_name
 
-    def create_task(self, task, tag=None):
+        if DISCOVERY_URL is not None:
+            self.task_api = build("taskqueue", "v1beta2", discoveryServiceUrl=DISCOVERY_URL)
+        else:
+            self.task_api = build("taskqueue", "v1beta2")
+
+    def _get_project_name(self, is_write):
+        """ Get the project name based for write command.
+        Args:
+            is_write: A boolean determining if the name will be used
+                for a write operation.
+        Returns:
+            A string of the project name required for a task_api call.
+        """
+        if is_write:
+            return "b~" + self.project_name
+        return self.project_name
+
+    def create_task(self, task_payload, tag=None):
         """Create a new task and places it on the taskqueue.
 
         Args:
-            task: A dicitonary describing the task.
+            task_payload: A dicitonary describing the task.
             tag: A tag attached to the task.
         Returns:
             The task id of the created task, otherwise None if error.
@@ -73,14 +92,14 @@ class QueueHandler(object):
                 task["tag"] = tag
 
             insert_request = self.task_api.tasks().insert(
-                project=self.project_name,
+                project=self._get_project_name(True),
                 taskqueue=self.taskqueue_name,
                 body=task
             )
             result = insert_request.execute()
-            return result['id']
+            return result["id"]
         except HttpError as http_error:
-            logging.error('Error during insert request: {}'.format(http_error))
+            logging.error("Error during insert request: {}".format(http_error))
             return None
 
     def lease_tasks(self, tasks_to_fetch, lease_secs):
@@ -90,13 +109,13 @@ class QueueHandler(object):
             tasks_to_fetch: The number of tasks to fetch.
             lease_secs: The number of seconds to lease for.
         Returns:
-            A Google Task as a dicitonary with the user defined
-            task (dictionary) under that 'task' key.
+            A Google Task as a dictionary with the user defined
+            task (dictionary) under that 'payload' key.
         """
         try:
             tasks = []
             lease_request = self.task_api.tasks().lease(
-                project=self.project_name,
+                project=self._get_project_name(True),
                 taskqueue=self.taskqueue_name,
                 leaseSecs=lease_secs,
                 numTasks=tasks_to_fetch
@@ -104,36 +123,60 @@ class QueueHandler(object):
             result = lease_request.execute()
             if result["kind"] == "taskqueue#tasks":
                 for task in result['items']:
-                    task['task'] = decode_dictionary(result['payloadBase64'])
+                    task["payload"] = decode_dictionary(result["payloadBase64"])
                     tasks.append(task)
             elif result["kind"] == "taskqueue#task":
-                task['task'] = decode_dictionary(result['payloadBase64'])
+                task["payload"] = decode_dictionary(result["payloadBase64"])
                 tasks.append(task)
             return tasks
         except HttpError as http_error:
-            logging.error('Error during lease request: {}'.format(http_error))
+            logging.error("Error during lease request: {}".format(http_error))
             return None
 
-    def delete_tasks(self, task_ids):
-        """Delete tasks from the taskqueue.
+    def update_task(self, task_id, new_lease_secs):
+        """Update a task lease from the taskqueue.
 
         Args:
-            task_ids: An array of tasks ids to be deleted.
+            task_id: A string of the task_id.
+            new_lease_secs: The number of seconds to update the lease
+                by.
         Returns:
-            An array of booleans determining if the task was
-            deleted successfully.
+            The updated Google Task as a dictionary, the payload is
+            untouched. If there is an error None is returned.
         """
-        is_deleted = []
-        for task_id in task_ids:
-            try:
-                delete_request = self.task_api.tasks().delete(
-                    project=self.project_name,
-                    taskqueue=self.taskqueue,
-                    task=task_id
-                )
-                result = delete_request.execute()
-                is_deleted.append(result.body == '')
-            except HttpError as http_error:
-                logging.error('Error during delete request: {}'.format(http_error))
-                is_deleted.append(False)
-        return is_deleted
+        try:
+            task = {
+                "queueName": self.taskqueue_name
+            }
+            patch_request = self.task_api.tasks().patch(
+                project=self._get_project_name(True),
+                taskqueue=self.taskqueue_name,
+                newLeaseSeconds=new_lease_secs,
+                task=task_id,
+                body=task
+            )
+            result = patch_request.execute()
+            return result
+        except HttpError as http_error:
+            logging.error("Error during lease request: {}".format(http_error))
+            return None
+
+    def delete_task(self, task_id):
+        """Delete a task from the taskqueue.
+
+        Args:
+            task_id: A string of the task_id.
+        Returns:
+            True if the delete was successful, False otherwise.
+        """
+        try:
+            delete_request = self.task_api.tasks().delete(
+                project=self._get_project_name(True),
+                taskqueue=self.taskqueue,
+                task=task_id
+            )
+            result = delete_request.execute()
+            return True
+        except HttpError as http_error:
+            logging.error("Error during delete request: {}".format(http_error))
+        return False
--- render/webserver.py
+++ render/webserver.py
@@ -10,93 +10,14 @@ from base64 import b64encode
 
 DEBUG = not int(os.getenv("FLASK_PRODUCTION", 1))
 PORT = int(os.getenv("PORT", 8080))
-DISCOVERY_URL = os.getenv("API_DISCOVERY_URL", None)
 
 application = Flask(__name__)
-task_api = None
-
-
-def load_api():
-    """TEST METHOD, NOT INVITED TO FUTURE ENDEAVOURS."""
-    global task_api
-    if task_api is None:
-        if DISCOVERY_URL is not None:
-            task_api = build("taskqueue", "v1beta2", discoveryServiceUrl=DISCOVERY_URL)
-        else:
-            task_api = build("taskqueue", "v1beta2")
-    return task_api
 
 
 @application.route("/")
 def index():
     """Get an index page describing the service."""
     return "CS-Unplugged - Render Engine"
-
-
-@application.route("/get/<file>")
-def get(file=None):
-    """Retrieve a file after generation.
-
-    Args:
-        file: An identier associated with the file.
-    Returns:
-        The file with 200 status or a 204 if not found.
-    """
-    pass  # TODO
-
-
-@application.route("/add/<item>")
-def add(item=None):
-    """TEST METHOD, NOT INVITED TO FUTURE ENDEAVOURS."""
-    task_api = load_api()
-    try:
-        encoded_string = b64encode(item.encode("ascii")).decode()
-        task = {
-          "kind": "taskqueues#task",
-          "queueName": "render-queue",
-          "payloadBase64": encoded_string
-        }
-        insert_request = task_api.tasks().insert(
-          project="b~cs-unplugged-develop",
-          taskqueue="render-queue",
-          body=task
-        )
-        result = insert_request.execute()
-        return json.dumps(result), 200
-    except HttpError as http_error:
-        return "Error", 500
-
-
-@application.route("/list")
-def list():
-    """TEST METHOD, NOT INVITED TO FUTURE ENDEAVOURS."""
-    task_api = load_api()
-    try:
-        list_request = task_api.tasks().list(
-          project="b~cs-unplugged-develop",
-          taskqueue="render-queue"
-        )
-        result = list_request.execute()
-        return json.dumps(result), 200
-    except HttpError as http_error:
-        return "Error", 500
-
-
-@application.route("/lease")
-def lease():
-    """TEST METHOD, NOT INVITED TO FUTURE ENDEAVOURS."""
-    task_api = load_api()
-    try:
-        lease_request = task_api.tasks().lease(
-          project="b~cs-unplugged-develop",
-          taskqueue="render-queue",
-          numTasks=1,
-          leaseSecs=3600
-        )
-        result = lease_request.execute()
-        return json.dumps(result), 200
-    except HttpError as http_error:
-        return "Error", 500
 
 
 @application.errorhandler(500)
