cb7bae35dac773f2c19a6c2d116204f9bdb65a6e
Jack Morgan <jackmorgannz@gmail.com>
2017-05-19 20:48:29 +1200

    Add tests for binary cards small resource
    Also adds utility functions

--- csunplugged/tests/resources/views/test_binary_cards_small.py
+++ csunplugged/tests/resources/views/test_binary_cards_small.py
@@ -2,22 +2,23 @@ import itertools
 from django.urls import reverse
 from tests.BaseTestWithDB import BaseTestWithDB
 from tests.resources.ResourcesTestDataGenerator import ResourcesTestDataGenerator
+from utils.import_resource_module import import_resource_module
 from utils.create_query_string import query_string
 
 
-class BinaryWindowsResourceViewTest(BaseTestWithDB):
+class BinaryCardsSmallResourceViewTest(BaseTestWithDB):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.test_data = ResourcesTestDataGenerator()
         self.language = "en"
 
-    def test_binary_windows_resource_form_view(self):
+    def test_binary_cards_small_resource_form_view(self):
         resource = self.test_data.create_test_resource(
-            "binary-windows",
-            "Binary Windows",
-            "resources/binary-windows.html",
-            "binary_windows.py",
+            "binary-cards",
+            "Binary Cards (small)",
+            "resources/binary-cards-small.html",
+            "binary_cards_small.py",
         )
         kwargs = {
             "resource_slug": resource.slug,
@@ -26,25 +27,20 @@ class BinaryWindowsResourceViewTest(BaseTestWithDB):
         response = self.client.get(url)
         self.assertEqual(200, response.status_code)
 
-    def test_binary_windows_resource_generation_valid_configurations(self):
+    def test_binary_cards_small_resource_generation_valid_configurations(self):
         resource = self.test_data.create_test_resource(
-            "binary-windows",
-            "Binary Windows",
-            "resources/binary-windows.html",
-            "binary_windows.py",
+            "binary-cards",
+            "Binary Cards (small)",
+            "resources/binary-cards-small.html",
+            "binary_cards_small.py",
         )
         kwargs = {
             "resource_slug": resource.slug,
         }
         base_url = reverse("resources:generate", kwargs=kwargs)
-
-        valid_options = {
-            "number_bits": ["4", "8"],
-            "value_type": ["binary", "lightbulb"],
-            "dot_counts": ["yes", "no"],
-            "paper_size": ["a4", "letter"],
-            "header_text": ["", "Example header"],
-        }
+        resource_module = import_resource_module(resource)
+        valid_options = resource_module.valid_options()
+        valid_options["header_text"] = ["", "Example header"]
         valid_option_keys = sorted(valid_options)
         combinations = [dict(zip(valid_option_keys, product)) for product in itertools.product(*(valid_options[valid_option_key] for valid_option_key in valid_option_keys))]  # noqa: E501
         print()
@@ -54,127 +50,131 @@ class BinaryWindowsResourceViewTest(BaseTestWithDB):
             response = self.client.get(url)
             self.assertEqual(200, response.status_code)
             if combination["dot_counts"] == "yes":
-                count_text = "with dot counts"
+                display_numbers_text = "with dot counts"
             else:
-                count_text = "without dot counts"
-            TEMPLATE = "{num_bits} bits - {value} - {counts}"
-            subtitle = TEMPLATE.format(
-                num_bits=combination["number_bits"],
-                value=combination["value_type"],
-                counts=count_text
+                display_numbers_text = "without dot counts"
+            if combination["black_back"] == "yes":
+                black_back_text = "with black back"
+            else:
+                black_back_text = "without black back"
+            subtitle = "{} bits - {} - {} - {}".format(
+                combination["number_bits"],
+                display_numbers_text,
+                black_back_text,
+                combination["paper_size"],
             )
             self.assertEqual(
                 response.get("Content-Disposition"),
-                'attachment; filename="Resource Binary Windows ({subtitle}).pdf"'.format(subtitle=subtitle)
+                'attachment; filename="Resource Binary Cards (small) ({subtitle}).pdf"'.format(subtitle=subtitle)
             )
             print("ok")
 
-    def test_binary_windows_resource_generation_missing_dot_count_parameter(self):
+    def test_binary_cards_small_resource_generation_missing_dot_count_parameter(self):
         resource = self.test_data.create_test_resource(
-            "binary-windows",
-            "Binary Windows",
-            "resources/binary-windows.html",
-            "binary_windows.py",
+            "binary-cards",
+            "Binary Cards (small)",
+            "resources/binary-cards-small.html",
+            "binary_cards_small.py",
         )
         kwargs = {
             "resource_slug": resource.slug,
         }
         url = reverse("resources:generate", kwargs=kwargs)
         get_parameters = {
-            "number_bits": "8",
-            "value_type": "binary",
-            "paper_size": "a4",
-            "header_text": "Example header text",
-        }
-        url += query_string(get_parameters)
-        response = self.client.get(url)
-        self.assertEqual(404, response.status_code)
-
-    def test_binary_windows_resource_generation_missing_number_bits_parameter(self):
-        resource = self.test_data.create_test_resource(
-            "binary-windows",
-            "Binary Windows",
-            "resources/binary-windows.html",
-            "binary_windows.py",
-        )
-        kwargs = {
-            "resource_slug": resource.slug,
-        }
-        url = reverse("resources:generate", kwargs=kwargs)
-        get_parameters = {
-            "dot_counts": "yes",
-            "value_type": "binary",
-            "paper_size": "a4",
-            "header_text": "Example header text",
-        }
-        url += query_string(get_parameters)
-        response = self.client.get(url)
-        self.assertEqual(404, response.status_code)
-
-    def test_binary_windows_resource_generation_missing_value_type_parameter(self):
-        resource = self.test_data.create_test_resource(
-            "binary-windows",
-            "Binary Windows",
-            "resources/binary-windows.html",
-            "binary_windows.py",
-        )
-        kwargs = {
-            "resource_slug": resource.slug,
-        }
-        url = reverse("resources:generate", kwargs=kwargs)
-        get_parameters = {
-            "dot_counts": "yes",
-            "number_bits": "8",
-            "paper_size": "a4",
-            "header_text": "Example header text",
-        }
-        url += query_string(get_parameters)
-        response = self.client.get(url)
-        self.assertEqual(404, response.status_code)
-
-    def test_binary_windows_resource_generation_missing_paper_size_parameter(self):
-        resource = self.test_data.create_test_resource(
-            "binary-windows",
-            "Binary Windows",
-            "resources/binary-windows.html",
-            "binary_windows.py",
-        )
-        kwargs = {
-            "resource_slug": resource.slug,
-        }
-        url = reverse("resources:generate", kwargs=kwargs)
-        get_parameters = {
-            "dot_counts": "yes",
-            "number_bits": "8",
-            "value_type": "binary",
-            "header_text": "Example header text",
-        }
-        url += query_string(get_parameters)
-        response = self.client.get(url)
-        self.assertEqual(404, response.status_code)
-
-    def test_binary_windows_resource_generation_missing_header_text_parameter(self):
-        resource = self.test_data.create_test_resource(
-            "binary-windows",
-            "Binary Windows",
-            "resources/binary-windows.html",
-            "binary_windows.py",
-        )
-        kwargs = {
-            "resource_slug": resource.slug,
-        }
-        url = reverse("resources:generate", kwargs=kwargs)
-        get_parameters = {
-            "dot_counts": "yes",
-            "number_bits": "8",
-            "value_type": "lightbulb",
+            "number_bits": "4",
+            "black_back": "yes",
             "paper_size": "a4",
             "header_text": "",
         }
         url += query_string(get_parameters)
         response = self.client.get(url)
+        self.assertEqual(404, response.status_code)
+
+    def test_binary_cards_small_resource_generation_missing_number_bits_parameter(self):
+        resource = self.test_data.create_test_resource(
+            "binary-cards",
+            "Binary Cards (small)",
+            "resources/binary-cards-small.html",
+            "binary_cards_small.py",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "dot_counts": "yes",
+            "black_back": "yes",
+            "paper_size": "a4",
+            "header_text": "",
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(404, response.status_code)
+
+    def test_binary_cards_small_resource_generation_missing_black_back_parameter(self):
+        resource = self.test_data.create_test_resource(
+            "binary-cards",
+            "Binary Cards (small)",
+            "resources/binary-cards-small.html",
+            "binary_cards_small.py",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "number_bits": "4",
+            "dot_counts": "yes",
+            "paper_size": "a4",
+            "header_text": "",
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(404, response.status_code)
+
+    def test_binary_cards_small_resource_generation_missing_paper_size_parameter(self):
+        resource = self.test_data.create_test_resource(
+            "binary-cards",
+            "Binary Cards (small)",
+            "resources/binary-cards-small.html",
+            "binary_cards_small.py",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "number_bits": "4",
+            "dot_counts": "yes",
+            "black_back": "yes",
+            "header_text": "",
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(404, response.status_code)
+
+    def test_binary_cards_small_resource_generation_missing_header_text_parameter(self):
+        resource = self.test_data.create_test_resource(
+            "binary-cards",
+            "Binary Cards (small)",
+            "resources/binary-cards-small.html",
+            "binary_cards_small.py",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "dot_counts": "yes",
+            "number_bits": "4",
+            "black_back": "yes",
+            "paper_size": "a4",
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
         self.assertEqual(200, response.status_code)
+        filename = "Resource Binary Cards (small) (4 bits - with dot counts - with black back - a4).pdf"
         self.assertEqual(
             response.get("Content-Disposition"),
-            'attachment; filename="Resource Binary Windows (8 bits - lightbulb - with dot counts).pdf"'
+            'attachment; filename="{}"'.format(filename)
         )
