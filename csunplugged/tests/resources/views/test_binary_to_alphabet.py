80bfba2d3e8b3048217f645428c83517304c44cb
Hayley van Waas <hayleyavw@gmail.com>
2017-05-23 14:20:06 +1200

    Removed redundant template element. Added tests for missing heading.

--- csunplugged/tests/resources/views/test_binary_to_alphabet.py
+++ csunplugged/tests/resources/views/test_binary_to_alphabet.py
@@ -38,7 +38,8 @@ class BinaryToAlphabetResourceViewTest(BaseTestWithDB):
         url = reverse("resources:generate", kwargs=kwargs)
         get_parameters = {
             "worksheet_version": "student",
-            "paper_size": "a4"
+            "paper_size": "a4",
+            "header_text": ""
         }
         url += query_string(get_parameters)
         response = self.client.get(url)
@@ -61,7 +62,8 @@ class BinaryToAlphabetResourceViewTest(BaseTestWithDB):
         url = reverse("resources:generate", kwargs=kwargs)
         get_parameters = {
             "worksheet_version": "student",
-            "paper_size": "letter"
+            "paper_size": "letter",
+            "header_text": ""
         }
         url += query_string(get_parameters)
         response = self.client.get(url)
@@ -84,7 +86,8 @@ class BinaryToAlphabetResourceViewTest(BaseTestWithDB):
         url = reverse("resources:generate", kwargs=kwargs)
         get_parameters = {
             "worksheet_version": "teacher",
-            "paper_size": "a4"
+            "paper_size": "a4",
+            "header_text": ""
         }
         url += query_string(get_parameters)
         response = self.client.get(url)
@@ -107,7 +110,31 @@ class BinaryToAlphabetResourceViewTest(BaseTestWithDB):
         url = reverse("resources:generate", kwargs=kwargs)
         get_parameters = {
             "worksheet_version": "teacher",
-            "paper_size": "letter"
+            "paper_size": "letter",
+            "header_text": ""
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(
+            response.get("Content-Disposition"),
+            'attachment; filename="Resource Binary To Alphabet (teacher - letter).pdf"'
+        )
+
+    def test_binary_to_alphabet_resource_generation_missing_header_text_parameter(self):
+        resource = self.test_data.create_test_resource(
+            "binary-to-alphabet",
+            "Binary To Alphabet",
+            "resources/binary-to-alphabet.html",
+            "binary_to_alphabet.py"
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "worksheet_version": "teacher",
+            "paper_size": "letter",
         }
         url += query_string(get_parameters)
         response = self.client.get(url)
@@ -129,7 +156,8 @@ class BinaryToAlphabetResourceViewTest(BaseTestWithDB):
         }
         url = reverse("resources:generate", kwargs=kwargs)
         get_parameters = {
-            "paper_size": "letter"
+            "paper_size": "letter",
+            "header_text": ""
         }
         url += query_string(get_parameters)
         response = self.client.get(url)
@@ -148,6 +176,7 @@ class BinaryToAlphabetResourceViewTest(BaseTestWithDB):
         url = reverse("resources:generate", kwargs=kwargs)
         get_parameters = {
             "worksheet_version": "student",
+            "header_text": ""
         }
         url += query_string(get_parameters)
         response = self.client.get(url)
