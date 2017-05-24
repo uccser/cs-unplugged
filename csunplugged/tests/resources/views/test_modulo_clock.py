494bb1a23de7239899e42082e64b481828dc7400
Hayley van Waas <hayleyavw@gmail.com>
2017-05-24 13:33:49 +1200

    Added tests for when header text is given

--- csunplugged/tests/resources/views/test_modulo_clock.py
+++ csunplugged/tests/resources/views/test_modulo_clock.py
@@ -27,7 +27,7 @@ class ModuloClockResourceViewTest(BaseTestWithDB):
         response = self.client.get(url)
         self.assertEqual(200, response.status_code)
 
-    def test_modulo_clock_resource_generation_2_a4(self):
+    def test_modulo_clock_resource_generation_2_a4_no_header_text(self):
         resource = self.test_data.create_test_resource(
             "modulo-clock",
             "Modulo Clock",
@@ -52,7 +52,7 @@ class ModuloClockResourceViewTest(BaseTestWithDB):
             'attachment; filename="{}"'.format(filename)
         )
 
-    def test_modulo_clock_resource_generation_2_letter(self):
+    def test_modulo_clock_resource_generation_2_letter_no_header_text(self):
         resource = self.test_data.create_test_resource(
             "modulo-clock",
             "Modulo Clock",
@@ -77,7 +77,7 @@ class ModuloClockResourceViewTest(BaseTestWithDB):
             'attachment; filename="{}"'.format(filename)
         )
 
-    def test_modulo_clock_resource_generation_10_a4(self):
+    def test_modulo_clock_resource_generation_10_a4_no_header_text(self):
         resource = self.test_data.create_test_resource(
             "modulo-clock",
             "Modulo Clock",
@@ -102,7 +102,7 @@ class ModuloClockResourceViewTest(BaseTestWithDB):
             'attachment; filename="{}"'.format(filename)
         )
 
-    def test_modulo_clock_resource_generation_10_letter(self):
+    def test_modulo_clock_resource_generation_10_letter_no_header_text(self):
         resource = self.test_data.create_test_resource(
             "modulo-clock",
             "Modulo Clock",
@@ -117,6 +117,106 @@ class ModuloClockResourceViewTest(BaseTestWithDB):
             "modulo_number": "10",
             "paper_size": "letter",
             "header_text": ""
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+        filename = "Resource Modulo Clock (10 - letter).pdf"
+        self.assertEqual(
+            response.get("Content-Disposition"),
+            'attachment; filename="{}"'.format(filename)
+        )
+
+    def test_modulo_clock_resource_generation_2_a4_header_text(self):
+        resource = self.test_data.create_test_resource(
+            "modulo-clock",
+            "Modulo Clock",
+            "resources/modulo-clock.html",
+            "modulo_clock.py",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "modulo_number": "2",
+            "paper_size": "a4",
+            "header_text": "Modulo Clock"
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+        filename = "Resource Modulo Clock (2 - a4).pdf"
+        self.assertEqual(
+            response.get("Content-Disposition"),
+            'attachment; filename="{}"'.format(filename)
+        )
+
+    def test_modulo_clock_resource_generation_2_letter_header_text(self):
+        resource = self.test_data.create_test_resource(
+            "modulo-clock",
+            "Modulo Clock",
+            "resources/modulo-clock.html",
+            "modulo_clock.py",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "modulo_number": "2",
+            "paper_size": "letter",
+            "header_text": ""
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+        filename = "Resource Modulo Clock (2 - letter).pdf"
+        self.assertEqual(
+            response.get("Content-Disposition"),
+            'attachment; filename="{}"'.format(filename)
+        )
+
+    def test_modulo_clock_resource_generation_10_a4_header_text(self):
+        resource = self.test_data.create_test_resource(
+            "modulo-clock",
+            "Modulo Clock",
+            "resources/modulo-clock.html",
+            "modulo_clock.py",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "modulo_number": "10",
+            "paper_size": "a4",
+            "header_text": "Modulo Clock"
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+        filename = "Resource Modulo Clock (10 - a4).pdf"
+        self.assertEqual(
+            response.get("Content-Disposition"),
+            'attachment; filename="{}"'.format(filename)
+        )
+
+    def test_modulo_clock_resource_generation_10_letter_header_text(self):
+        resource = self.test_data.create_test_resource(
+            "modulo-clock",
+            "Modulo Clock",
+            "resources/modulo-clock.html",
+            "modulo_clock.py",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "modulo_number": "10",
+            "paper_size": "letter",
+            "header_text": "Modulo Clock"
         }
         url += query_string(get_parameters)
         response = self.client.get(url)
