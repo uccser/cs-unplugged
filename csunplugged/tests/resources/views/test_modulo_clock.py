a8035fd621971788cd6d8b9f8d66b1d00b0e1a8e
Hayley van Waas <hayleyavw@gmail.com>
2017-05-24 12:51:03 +1200

    Added tests for missing parameters

--- csunplugged/tests/resources/views/test_modulo_clock.py
+++ csunplugged/tests/resources/views/test_modulo_clock.py
@@ -12,4 +12,80 @@ class ModuloClockResourceViewTest(BaseTestWithDB):
         self.language = "en"
 
     def test_modulo_clock_resource_form_view(self):
+        resource = self.test_data.create_test_resource(
+            "modulo-clock",
+            "Modulo Clock",
+            "resources/modulo-clock.html",
+            "modulo_clock.py",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:resource", kwargs=kwargs)
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+
+    def test_modulo_clock_resource_generation_valid_configurations(self):
         pass
+
+    def test_modulo_clock_resource_generation_missing_modulo_number_parameter(self):
+        resource = self.test_data.create_test_resource(
+            "modulo-clock",
+            "Modulo Clock",
+            "resources/modulo-clock.html",
+            "modulo_clock.py",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "paper_size": "a4",
+            "header_text": "",
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(404, response.status_code)
+
+    def test_modulo_clock_resource_generation_missing_paper_size_parameter(self):
+        resource = self.test_data.create_test_resource(
+            "modulo-clock",
+            "Modulo Clock",
+            "resources/modulo-clock.html",
+            "modulo_clock.py",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "modulo_number": "2",
+            "header_text": "",
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(404, response.status_code)
+
+    def test_modulo_clock_resource_generation_missing_header_text_parameter(self):
+        resource = self.test_data.create_test_resource(
+            "modulo-clock",
+            "Modulo Clock",
+            "resources/modulo-clock.html",
+            "modulo_clock.py",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "modulo_number": "2",
+            "paper_size": "a4",
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+        filename = "Resource Modulo Clock (2 - a4).pdf"
+        self.assertEqual(
+            response.get("Content-Disposition"),
+            'attachment; filename="{}"'.format(filename)
+        )
