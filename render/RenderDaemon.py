b676e4ed7630f680c386ba9bd5c9631b26805ba3
ravenmaster_001 <hayden.adam.jackson@gmail.com>
2017-05-26 14:01:11 +1200

    WIP Refactor document rendering, outlining of cloudstorage manager.

--- render/RenderDaemon.py
+++ render/RenderDaemon.py
@@ -3,9 +3,17 @@ import os
 import time
 import signal
 import logging
+import importlib
+from io import BytesIO
+from PIL import Image
+from base64 import b64encode
+from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML, CSS
 from daemons.prefab.run import RunDaemon
 from QueueHandler import QueueHandler
+from ResourceManager import ResourceManager
 
+# Daemon Setup and Task Management Constants
 PROJECT_NAME = os.getenv("PROJECT_NAME", None)
 QUEUE_NAME = os.getenv("QUEUE_NAME", None)
 DISCOVERY_URL = os.getenv("API_DISCOVERY_URL", None)
@@ -18,6 +26,11 @@ TASK_RETRY_LIMIT = int(os.getenv("TASK_RETRY_LIMIT", 5))
 RENDER_SLEEP_TIME = float(os.getenv("RENDER_SLEEP_TIME", 10))
 
 logger = logging.getLogger(__name__)
+
+# File Generation and Processing Constants
+MM_TO_PIXEL_RATIO = 3.78
+A4_MM_SCALE = 267
+LETTER_MM_SCALE = 249
 
 
 def handle_timelimit_exceeded():
@@ -36,6 +49,12 @@ class RenderDaemon(RunDaemon):
         """
         super(RenderDaemon, self).__init__(*args, **kwargs)
         self.handle(signal.SIGALRM, handle_timelimit_exceeded)
+        self.resource_manager = ResourceManager(".")
+        self.cache_manager = ResourceManager("./cache")
+        self.template_environment = Environment(
+            loader=FileSystemLoader('/render/templates'),
+            autoescape=False
+        )
         # Handle SIGUSR1 for closing up for pre-emption.
 
     def run(self):
@@ -77,6 +96,8 @@ class RenderDaemon(RunDaemon):
         task = task_descriptor["payload"]
         task_kind = task["kind"]
 
+        filename, document = self.generate_resource_pdf(task)
+
         if task_kind == "task#render":
             try:
                 # TODO: save then delete
@@ -86,3 +107,97 @@ class RenderDaemon(RunDaemon):
                 queue.delete_task(task_id)
         else:
             raise Exception("Unrecognized task: {}.".format(task_kind))
+
+    # Task needs:
+
+    def generate_resource_pdf(self, task):
+        """Return a response containing a generated PDF resource.
+
+        Args:
+            task: A dicitionary of values specifying the task.
+                Must have:
+                  - filename
+                  - header_text
+                  - paper_size
+                  - copies
+                  - resource_slug
+                  - resource_name
+                  - resource_view
+                  - url
+
+        Returns:
+            Tuple of PDF file of generated resource and filename.
+        """
+        if task["paper_size"] is None:
+            raise Exception()  # TODO
+
+        module_path = "resources.{}".format(task["resource_view"])
+        resource_image_generator = importlib.import_module(module_path)
+
+        context = dict()
+        context["resource_name"] = task["resource_name"]
+        context["header_text"] = task["header_text"]
+        context["url"] = task["url"]
+
+        context["resource_images"] = []
+        for copy in range(0, context["copies"]):
+            context["resource_images"].append(
+                self.generate_resource_image(context, resource_image_generator)
+            )
+
+        filename = "{} ({})".format(task["resource_name"], resource_image_generator.subtitle(context))
+        context["filename"] = filename
+
+        template_filename = task.get("template", "base-resource-pdf.html")
+        css_filename = task.get("css", "css/print-resource-pdf.css")
+
+        template = self.template_environment.get_template(template_filename)
+        pdf_html = template.render(context)  # TODO: Future consider async
+        html = HTML(string=pdf_html, base_url=task["base_url"])
+        css_string = self.resource_manager.load(css_filename, encoding="UTF-8")
+        base_css = CSS(string=css_string)
+        return filename, html.write_pdf(stylesheets=[base_css])
+
+    def generate_resource_image(self, task, resource_image_generator):
+        """Retrieve image(s) for one copy of resource from resource generator.
+
+        Images are resized to size.
+
+        Args:
+            task: The specification of file to generate as a dictionary.
+            resource_image_generator: The file generation module.
+
+        Returns:
+            List of Base64 strings of a generated resource images for one copy.
+        """
+        # Get images from resource image creator
+        raw_images = resource_image_generator.resource_image(task, self.resource_manager)
+        if not isinstance(raw_images, list):
+            raw_images = [raw_images]
+
+        # Resize images to reduce file size
+        max_pixel_height = 0
+        if task["paper_size"].lower() == "a4":
+            max_pixel_height = A4_MM_SCALE * MM_TO_PIXEL_RATIO
+        elif task["paper_size"].lower() == "letter":
+            max_pixel_height = LETTER_MM_SCALE * MM_TO_PIXEL_RATIO
+        else:
+            raise Exception()  # TODO
+
+        images = []
+        for image in raw_images:
+            width, height = image.size
+            if height > max_pixel_height:
+                ratio = max_pixel_height / height
+                width *= ratio
+                height *= ratio
+                image = image.resize((int(width), int(height)), Image.ANTIALIAS)
+
+            # Save image to buffer
+            image_buffer = BytesIO()
+            image.save(image_buffer, format="PNG")
+
+            # Add base64 of image to list of images
+            images.append(b64encode(image_buffer.getvalue()))
+
+        return images
---
+++ render/ResourceManager.py
@@ -0,0 +1,25 @@
+"""Handles access to external filesystems from the render service."""
+
+
+class ResourceManager(object):
+    """ """
+
+    def __init__(self):
+        """
+        """
+        pass
+
+    def load(self, filename, encoding):
+        """
+        """
+        pass
+
+    def save(self, filename):
+        """
+        """
+        pass
+
+    def list_resources(self, directory):
+        """
+        """
+        pass
