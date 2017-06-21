49b1dd502b94a5cf07c1ce664175a82dbca0728f
Hayley van Waas <hayleyavw@gmail.com>
2017-06-21 15:12:55 +1200

    Add tests for arrow and left right cards resources

--- csunplugged/tests/resources/urls/test_left_right_cards.py
+++ csunplugged/tests/resources/urls/test_left_right_cards.py
@@ -2,18 +2,18 @@ from tests.BaseTestWithDB import BaseTestWithDB
 from django.urls import reverse
 
 
-class JobBadgesResourceURLTest(BaseTestWithDB):
+class LeftRightCardsResourceURLTest(BaseTestWithDB):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.language = "en"
-        self.RESOURCE_SLUG = "job-badges"
+        self.RESOURCE_SLUG = "left-right-cards"
         self.RESOURCE_URL_KWARGS = {"resource_slug": self.RESOURCE_SLUG}
 
-    def test_valid_job_badges_resource_url(self):
+    def test_valid_left_right_cards_resource_url(self):
         url = reverse("resources:resource", kwargs=self.RESOURCE_URL_KWARGS)
-        self.assertEqual(url, "/en/resources/job-badges/")
+        self.assertEqual(url, "/en/resources/left-right-cards/")
 
-    def test_valid_job_badges_resource_generate_url(self):
+    def test_valid_left_right_cards_resource_generate_url(self):
         url = reverse("resources:generate", kwargs=self.RESOURCE_URL_KWARGS)
-        self.assertEqual(url, "/en/resources/job-badges/generate")
+        self.assertEqual(url, "/en/resources/left-right-cards/generate")
