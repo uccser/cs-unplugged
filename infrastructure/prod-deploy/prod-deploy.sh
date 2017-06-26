0dbbafaed99dce56097b000569bd4cf69e3c9320
Jack Morgan <jackmorgannz@gmail.com>
2017-06-26 19:17:10 +1200

    Update installation steps for Google Cloud SDK

--- infrastructure/prod-deploy/prod-deploy.sh
+++ infrastructure/prod-deploy/prod-deploy.sh
@@ -21,8 +21,17 @@ pip install -r ./requirements/production.txt
 # and choose default settings.
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1;
 
-# Install base Google Cloud SDK to use gcloud command tool.
-curl https://sdk.cloud.google.com | bash;
+# Create an environment variable for the correct distribution.
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
+# Add the Cloud SDK distribution URI as a package source.
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key.
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# Update the package list and install the Cloud SDK.
+sudo apt-get update && sudo apt-get install google-cloud-sdk
 
 # Decrypt secret files archive that contain credentials.
 #
@@ -41,11 +50,6 @@ tar -xf prod-deploy-secrets.tar
 # Authenticate with gcloud tool using the decrypted service account credentials.
 # See: https://cloud.google.com/sdk/gcloud/reference/auth/activate-service-account
 gcloud auth activate-service-account --key-file continuous-deployment-prod.json
-
-# Peform an update of gcloud to latest version.
-# This installs the 'app' tool, for deploying to Google App Engine.
-# See: https://cloud.google.com/sdk/gcloud/reference/components/update
-sudo gcloud components update
 
 # Display the gcloud version, useful for debugging purposes.
 # See: https://cloud.google.com/sdk/gcloud/reference/version
