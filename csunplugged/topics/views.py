9a6185b06de343b46e9e1cb2cc5b0a4e5bfb630b
Jack Morgan <jackmorgannz@gmail.com>
2017-08-05 21:49:23 +1200

    Fix bug where order of lessons wasn't consistent

--- csunplugged/topics/views.py
+++ csunplugged/topics/views.py
@@ -318,6 +318,7 @@ class CurriculumIntegrationView(generic.DetailView):
         # Add in a QuerySet of all the prerequisite lessons
         context["prerequisite_lessons"] = self.object.prerequisite_lessons.select_related().order_by(
             "unit_plan__name",
+            "lessonnumber",
         )
         return context
 
