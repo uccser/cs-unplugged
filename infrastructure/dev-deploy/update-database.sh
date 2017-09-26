3de84d5fc177c74bccf43248f397358f5cf9784f
Jack Morgan <jackmorgannz@gmail.com>
2017-09-26 14:49:58 +1300

    Decrypt dev secrets intelligently (#590)
    Removes a lot of duplication in:
    - Decrypting of secret .tar.
    - Removes duplication of secret values and uses
      Python script to create files using secrets.

--- infrastructure/dev-deploy/update-database.sh
+++ infrastructure/dev-deploy/update-database.sh
@@ -6,18 +6,7 @@
 mv ./infrastructure/cloud-sql-proxy/docker-compose.yml ./docker-compose.yml
 
 # Decrypt secret files archive that contain credentials.
-#
-# This includes:
-#   - continuous-deployment-dev.json
-#     Google Cloud Platform Service Account for using with gcloud.
-#   - app-dev.yaml
-#     Google App Engine YAML file for deployment, contains sensitive data.
-#   - load-dev-deploy-envs.sh
-#     Loads environment variables used when running local Django.
-openssl aes-256-cbc -K "${encrypted_323d8adec5b7_key}" -iv "${encrypted_323d8adec5b7_iv}" -in ./infrastructure/dev-deploy/dev-deploy-secrets.tar.enc -out dev-deploy-secrets.tar -d
-
-# Unzip the decrypted secret archive into the current folder.
-tar -xf dev-deploy-secrets.tar
+./infrastructure/dev-deploy/decrypt-dev-secrets.sh
 
 # Load environment variables.
 source ./load-dev-deploy-envs.sh
