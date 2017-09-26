82992696acf66bb375a25eea6a52ce6273a3e89d
Jack Morgan <jackmorgannz@gmail.com>
2017-09-27 12:36:24 +1300

    Add missing steps to install and run Cloud SQL Proxy

--- infrastructure/dev-deploy/update-database.sh
+++ infrastructure/dev-deploy/update-database.sh
@@ -12,5 +12,23 @@ mv ./infrastructure/cloud-sql-proxy/docker-compose.yml ./docker-compose.yml
 source ./load-dev-deploy-envs.sh
 export GOOGLE_AUTH_JSON=`< ./continuous-deployment-dev.json`
 
+# Download the Google Cloud SQL proxy for updating development database.
+#
+# See: https://cloud.google.com/python/django/flexible-environment#install_the_sql_proxy
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64
+# Rename the proxy to standard filename.
+mv cloud_sql_proxy.linux.amd64 cloud_sql_proxy
+# Allow the proxy to be executable.
+chmod +x cloud_sql_proxy
+
+# Start the Google Cloud SQL Proxy.
+#
+# This connects to the database instance using the connection name set in an
+# environment variable. It is authenticated using the service account credentials.
+# The proxy command is appended with '&>/dev/null &' to run in the background
+# and to not send output to console.
+# See: https://cloud.google.com/python/django/flexible-environment#initialize_your_cloud_sql_instance
+./cloud_sql_proxy -instances="${GOOGLE_CLOUD_SQL_CONNECTION_NAME}"=tcp:5433 -credential_file="./continuous-deployment-dev.json" >/dev/null 2>/dev/null &
+
 # Start the system which runs the migrate and updatedata system commands.
 ./csu start
