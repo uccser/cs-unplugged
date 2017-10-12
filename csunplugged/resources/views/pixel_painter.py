d91441e76ce7658d882e991a9b06be580e915505
Jack Morgan <jackmorgannz@gmail.com>
2017-10-13 11:24:32 +1300

    Add page grid reference sheet to pixel painter

--- csunplugged/resources/views/pixel_painter.py
+++ csunplugged/resources/views/pixel_painter.py
@@ -3,6 +3,7 @@
 from PIL import Image, ImageDraw, ImageFont
 from utils.retrieve_query_parameter import retrieve_query_parameter
 from math import ceil
+from yattag import Doc
 import string
 
 def resource(request, resource):
@@ -39,14 +40,17 @@ def resource(request, resource):
     pages = []
     number_column_pages = ceil(image_width / COLUMNS_PER_PAGE)
     number_row_pages = ceil(image_height / ROWS_PER_PAGE)
-    LETTERS = string.ascii_uppercase
+    page_grid_coords = create_page_grid_coords(number_column_pages, number_row_pages)
 
-    # For each page column
-    for number_column_page in range(0, number_column_pages):
-        page_start_column = (number_column_page) * COLUMNS_PER_PAGE
-        # For each page row
-        for number_row_page in range(0, number_row_pages):
-            page_start_row = (number_row_page) * ROWS_PER_PAGE
+    grid_page = grid_reference_page(page_grid_coords, image_name)
+    pages.append({"type": "html", "data": grid_page})
+
+    # For each page row
+    for number_row_page in range(0, number_row_pages):
+        page_start_row = (number_row_page) * ROWS_PER_PAGE
+        # For each page column
+        for number_column_page in range(0, number_column_pages):
+            page_start_column = (number_column_page) * COLUMNS_PER_PAGE
             # Create page
             page = Image.new("RGB", (IMAGE_SIZE_X, IMAGE_SIZE_Y), "#fff")
             draw = ImageDraw.Draw(page)
@@ -54,7 +58,7 @@ def resource(request, resource):
             # Add page grid reference
             draw.text(
                 (LINE_WIDTH * 4, -4),
-                "{}{}".format(LETTERS[number_row_page], number_column_page + 1),
+                page_grid_coords[number_row_page][number_column_page],
                 font=FONT_SMALL,
                 fill=TEXT_COLOUR
             )
@@ -107,6 +111,65 @@ def resource(request, resource):
 
             pages.append({"type": "image", "data": page})
     return pages
+
+
+def create_page_grid_coords(columns, rows):
+    """Create a grid of page coordinates in a 2D array.
+
+    Example:
+        When asked for a grid for a 3x2 page grid, the result is:
+        grid = [
+         ["A1", "A2", "A3"],
+         ["B1", "B2", "B3"]
+        ]
+        grid[0][0] = "A1"
+        grid[0][1] = "A2"
+        grid[2][1] = "C2"
+
+    Args:
+        columns: Number of page columns (int).
+        rows: Number of page rows (int).
+
+    Returns:
+        A 2D list containing page grid references as strings (list).
+    """
+    LETTERS = string.ascii_uppercase
+    page_grid_coords = [[""] * columns for i in range(rows)]
+    for row in range(0, rows):
+        for column in range(0, columns):
+            page_grid_coords[row][column] = "{}{}".format(LETTERS[row], column + 1)
+    return page_grid_coords
+
+
+def grid_reference_page(page_grid_coords, image_name):
+    """Create page grid reference HTML.
+
+    Args:
+        page_grid_coords: A 2D list containing the page grid as strings (list).
+        image_name: The name of the image used in this resource (str).
+
+    Returns:
+        HTML string for the grid reference page.
+    """
+    doc, tag, text, line = Doc().ttl()
+    line("style", "#grid-table td {border:1px solid black;padding:1rem 0.5rem;}")
+    with tag("h1"):
+        text("Pixel Painter")
+    with tag("h2"):
+        text("Page grid reference for {} image".format(image_name))
+    with tag("p"):
+        text(
+            "Once pixels on each page are filled in correctly, ",
+            "cut each grid out and arrange in the following layout ",
+            "(page names are in the top right corner)."
+        )
+    with tag("table", id="grid-table"):
+        with tag("tbody"):
+            for row_num in range(0, len(page_grid_coords)):
+                with tag("tr"):
+                    for column_num in range(0, len(page_grid_coords[row_num])):
+                        line("td", page_grid_coords[row_num][column_num])
+    return doc.getvalue()
 
 
 def subtitle(request, resource):
