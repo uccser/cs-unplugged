28e3022f7e3ec7401b8d029c4fbaf0c44922483e
Jack Morgan <jackmorgannz@gmail.com>
2017-10-27 12:39:13 +1300

    Remove extra space

--- csunplugged/config/settings/base.py
+++ csunplugged/config/settings/base.py
@@ -161,7 +161,7 @@ TEMPLATES = [
 STATIC_ROOT = str(ROOT_DIR.path("staticfiles")) + "/"
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-BUILD_ROOT =  str(ROOT_DIR.path("build")) + "/"
+BUILD_ROOT = str(ROOT_DIR.path("build")) + "/"
 STATIC_URL = "/staticfiles/"
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
