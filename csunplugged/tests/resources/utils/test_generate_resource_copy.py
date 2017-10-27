bb7b770633335a3f4fa5e51ff9ddbbf2e4318bc9
Jack Morgan <jackmorgannz@gmail.com>
2017-10-27 17:52:20 +1300

    Add tests for resource image resizing

--- csunplugged/tests/resources/utils/test_generate_resource_copy.py
+++ csunplugged/tests/resources/utils/test_generate_resource_copy.py
@@ -7,6 +7,9 @@ from resources.utils.generate_resource_copy import generate_resource_copy
 from unittest.mock import MagicMock
 from utils.errors.ThumbnailPageNotFound import ThumbnailPageNotFound
 from utils.errors.MoreThanOneThumbnailPageFound import MoreThanOneThumbnailPageFound
+from io import BytesIO
+from PIL import Image
+import base64
 
 
 @tag("resource")
@@ -15,6 +18,9 @@ class GenerateResourceCopyTest(BaseTestWithDB):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.test_data = ResourcesTestDataGenerator()
+        self.MM_TO_PIXEL_RATIO = 6
+        self.A4_HEIGHT = 267
+        self.LETTER_HEIGHT = 249
 
     def test_generate_resources_copy_valid_generator(self):
         resource = self.test_data.create_resource(
@@ -70,3 +76,49 @@ class GenerateResourceCopyTest(BaseTestWithDB):
             generator,
             True
         )
+
+    def test_generate_resources_copy_image_a4_not_resized(self):
+        image = Image.new("1", (100, 100))
+        generator = MagicMock()
+        generator.data.return_value = [{"type": "image", "data": image}]
+        generator.requested_options = {"paper_size": "a4"}
+        copy = generate_resource_copy(generator, thumbnail=True)
+        copy_data = BytesIO(base64.b64decode(copy[0]["data"]))
+        copy_image = Image.open(copy_data)
+        self.assertEqual(image.size, copy_image.size)
+
+    def test_generate_resources_copy_image_a4_resized(self):
+        size = 3000
+        ratio = (self.A4_HEIGHT * self.MM_TO_PIXEL_RATIO) / size
+        expected_size = (int(size * ratio), int(size * ratio))
+        image = Image.new("1", (size, size))
+        generator = MagicMock()
+        generator.data.return_value = [{"type": "image", "data": image}]
+        generator.requested_options = {"paper_size": "a4"}
+        copy = generate_resource_copy(generator, thumbnail=True)
+        copy_data = BytesIO(base64.b64decode(copy[0]["data"]))
+        copy_image = Image.open(copy_data)
+        self.assertEqual(expected_size, copy_image.size)
+
+    def test_generate_resources_copy_image_letter_not_resized(self):
+        image = Image.new("1", (100, 100))
+        generator = MagicMock()
+        generator.data.return_value = [{"type": "image", "data": image}]
+        generator.requested_options = {"paper_size": "letter"}
+        copy = generate_resource_copy(generator, thumbnail=True)
+        copy_data = BytesIO(base64.b64decode(copy[0]["data"]))
+        copy_image = Image.open(copy_data)
+        self.assertEqual(image.size, copy_image.size)
+
+    def test_generate_resources_copy_image_letter_resized(self):
+        size = 3000
+        ratio = (self.LETTER_HEIGHT * self.MM_TO_PIXEL_RATIO) / size
+        expected_size = (int(size * ratio), int(size * ratio))
+        image = Image.new("1", (size, size))
+        generator = MagicMock()
+        generator.data.return_value = [{"type": "image", "data": image}]
+        generator.requested_options = {"paper_size": "letter"}
+        copy = generate_resource_copy(generator, thumbnail=True)
+        copy_data = BytesIO(base64.b64decode(copy[0]["data"]))
+        copy_image = Image.open(copy_data)
+        self.assertEqual(expected_size, copy_image.size)
