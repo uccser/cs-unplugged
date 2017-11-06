3f82de194298ef50fd5edf1f53f9052e8e8f25d6
Jack Morgan <jackmorgannz@gmail.com>
2017-11-07 11:50:32 +1300

    Add more pixel painter tests

--- csunplugged/tests/resources/views/test_pixel_painter.py
+++ csunplugged/tests/resources/views/test_pixel_painter.py
@@ -74,3 +74,88 @@ class PixelPainterResourceViewTest(BaseTestWithDB):
                 'attachment; filename="Resource Pixel Painter ({subtitle}).pdf"'.format(subtitle=subtitle)
             )
             print("ok")
+    def test_pixel_painter_resource_generation_missing_method_parameter(self):
+        resource = self.test_data.create_resource(
+            "pixel-painter",
+            "Pixel Painter",
+            "resources/pixel-painter.html",
+            "PixelPainterResourceGenerator",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "image": "boat",
+            "paper_size": "letter",
+            "header_text": "",
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(HTTPStatus.NOT_FOUND, response.status_code)
+
+    def test_pixel_painter_resource_generation_missing_image_parameter(self):
+        resource = self.test_data.create_resource(
+            "pixel-painter",
+            "Pixel Painter",
+            "resources/pixel-painter.html",
+            "PixelPainterResourceGenerator",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "method": "black-white",
+            "paper_size": "a4",
+            "header_text": "",
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(HTTPStatus.NOT_FOUND, response.status_code)
+
+    def test_pixel_painter_resource_generation_missing_paper_size_parameter(self):
+        resource = self.test_data.create_resource(
+            "pixel-painter",
+            "Pixel Painter",
+            "resources/pixel-painter.html",
+            "PixelPainterResourceGenerator",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "method": "black-white",
+            "image": "boat",
+            "header_text": "",
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(HTTPStatus.NOT_FOUND, response.status_code)
+
+    def test_pixel_painter_resource_generation_missing_header_text_parameter(self):
+        resource = self.test_data.create_resource(
+            "pixel-painter",
+            "Pixel Painter",
+            "resources/pixel-painter.html",
+            "PixelPainterResourceGenerator",
+        )
+        kwargs = {
+            "resource_slug": resource.slug,
+        }
+        url = reverse("resources:generate", kwargs=kwargs)
+        get_parameters = {
+            "method": "black-white",
+            "image": "boat",
+            "paper_size": "letter",
+            "header_text": "",
+        }
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(HTTPStatus.OK, response.status_code)
+        filename = "Resource Pixel Painter (Boat - Black and White - letter).pdf"
+        self.assertEqual(
+            response.get("Content-Disposition"),
+            'attachment; filename="{}"'.format(filename)
+        )
