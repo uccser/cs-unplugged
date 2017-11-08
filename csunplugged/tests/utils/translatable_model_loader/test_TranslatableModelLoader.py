e9998ce3b367d70a54a5c2f8150048e7be1634ee
Jordan Griffiths <jordangriffiths01@gmail.com>
2017-11-08 18:59:03 +1300

    Add test definitions for TranslatableModelLoader unit tests

---
+++ csunplugged/tests/utils/translatable_model_loader/test_TranslatableModelLoader.py
@@ -0,0 +1,57 @@
+"""Test class for TranslatableModelLoader."""
+
+from django.test import SimpleTestCase
+
+class TranslatableModelLoaderTest(SimpleTestCase):
+    """Test class for TranslatableModelLoader."""
+
+    def test_get_yaml_translations_english(self):
+        pass
+
+    def test_get_yaml_translations_english_missing_reqd_field(self):
+        pass
+
+    def test_get_yaml_translations_english_missing_reqd_slug(self):
+        pass
+
+    def test_get_yaml_translations_field_map(self):
+        pass
+
+    def test_get_yaml_translations_translated(self):
+        pass
+
+    def test_get_yaml_translations_translated_missing_reqd_field(self):
+        pass
+
+    def test_get_yaml_translations_translated_missing_reqd_slug(self):
+        pass
+
+    def test_get_yaml_translation_missing_yaml(self):
+        pass
+
+    def test_get_markdown_translations_english(self):
+        pass
+
+    def test_get_markdown_translation_english_missing_file(self):
+        pass
+
+    def test_get_markdown_translations_translated(self):
+        pass
+
+    def test_get_markdown_translation_translated_missing_file(self):
+        pass
+
+    def test_populate_translations(self):
+        pass
+
+    def test_mark_translation_availability_all_required_fields_present(self):
+        pass
+
+    def test_mark_translation_availability_required_field_missing(self):
+        pass
+
+    def test_mark_translation_availability_required_fields_not_given(self):
+        pass
+
+    def test_get_blank_translation_dictionary(self):
+        pass
