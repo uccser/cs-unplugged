154b1ac694fb96a5e4908e7586c019da50d05ba2
Jordan Griffiths <jordangriffiths01@gmail.com>
2017-11-12 13:10:51 +1300

    Add unit tests for translate_url template tag

--- csunplugged/config/templatetags/translate_url.py
+++ csunplugged/config/templatetags/translate_url.py
@@ -3,18 +3,32 @@
 from django.template import Library
 from django.core.urlresolvers import resolve, reverse
 from django.utils.translation import activate, get_language
+from django.template import TemplateSyntaxError
+from django.urls.exceptions import Resolver404
+
+from utils.language_utils import get_available_languages
+
+INVALID_ATTRIBUTE_MESSAGE = "The 'translate_url' tag was given an invalid language code '{}'."
+INVALID_PATH_MESSAGE = "The 'translate_url' tag could not resolve the current url {}."
+
 
 register = Library()
 
 
 @register.simple_tag(takes_context=True)
-def translate_url(context, lang=None, *args, **kwargs):
+def translate_url(context, lang, *args, **kwargs):
     """Get active page's url for a specified language.
 
     Usage: {% translate_url 'en' %}
     """
+    if lang not in get_available_languages():
+        raise TemplateSyntaxError(INVALID_ATTRIBUTE_MESSAGE.format(lang))
+
     url = context["request"].path
-    url_parts = resolve(url)
+    try:
+        url_parts = resolve(url)
+    except Resolver404:
+        raise TemplateSyntaxError(INVALID_PATH_MESSAGE.format(url))
 
     cur_language = get_language()
     try:
