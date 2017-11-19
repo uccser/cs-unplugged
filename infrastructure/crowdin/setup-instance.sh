ef178f38fa9608e8f7dd1c3adeb266764924574e
Jordan Griffiths <jordangriffiths01@gmail.com>
2017-11-20 01:03:36 +1300

    Refine setup-instance.sh, add crontab configuration

--- infrastructure/crowdin/setup-instance.sh
+++ infrastructure/crowdin/setup-instance.sh
@@ -5,23 +5,23 @@ SCRIPTS_DIR="crowdin_bot_scripts"
 PYTHON_PACKAGE_DIR="crowdin_bot_python_package"
 
 # Install packages
-apt-get install git --yes
-apt-get install default-jre --yes  # Java, for crowdin cli
-apt-get install python3-pip --yes
-apt-get install python3-lxml --yes  # Install here instead of pip3 because compilation uses too much RAM
-pip3 install -U pip setuptools
-pip3 install verto
+sudo apt-get install git --yes
+sudo apt-get install default-jre --yes  # Java, for crowdin cli
+sudo apt-get install python3-pip --yes
+sudo apt-get install python3-lxml --yes  # Install here instead of pip3 because compilation uses too much RAM
+sudo pip3 install -U pip setuptools
+sudo pip3 install verto pyyaml
 
 # Install crowdin cli
 wget -qO - https://artifacts.crowdin.com/repo/GPG-KEY-crowdin | sudo apt-key add -
-echo "deb https://artifacts.crowdin.com/repo/deb/ /" > /etc/apt/sources.list.d/crowdin.list
-apt-get update
-apt-get install crowdin --yes
+echo "deb https://artifacts.crowdin.com/repo/deb/ /" | sudo tee /etc/apt/sources.list.d/crowdin.list > /dev/null
+sudo apt-get update
+sudo apt-get install crowdin --yes
 
 # Install hub
 wget https://github.com/github/hub/releases/download/v2.2.5/hub-linux-amd64-2.2.5.tgz
 tar zvxvf hub-linux-amd64-2.2.5.tgz
-./hub-linux-amd64-2.2.5/install
+sudo ./hub-linux-amd64-2.2.5/install
 rm -rf hub-linux-amd64-2.2.5
 
 # Unzip crowdin-bot source
@@ -36,23 +36,20 @@ gcloud kms decrypt --ciphertext-file=crowdin_api_key.enc --plaintext-file=crowdi
 gcloud kms decrypt --ciphertext-file=gce_key.enc --plaintext-file=gce_key --location=global --keyring=csunplugged-keyring --key=uccser-bot-ssh-key
 
 # Add secret env vars to bashrc
-cat >> ~/.bashrc << EOF
+cat > crowdin-bot-env-secrets.sh << EOF
 export CROWDIN_API_KEY=$(cat crowdin_api_key)
 export GITHUB_TOKEN=$(cat uccser_bot_token)
 EOF
+sudo cp crowdin-bot-env-secrets.sh "../${SCRIPTS_DIR}"
 
-# Add github ssh key to ssh agent
-eval "$(ssh-agent -s)"
-cp gce_key ~/.ssh/id_rsa
-chmod 400 ~/.ssh/id_rsa
-# ...and add it to config file in case of restart
-# TODO: Fix this, it's not registering properly, so can't git clone
+# Setup github SSH key
+cp gce_key ~/.ssh/gce_key
+chmod 400 ~/.ssh/gce_key
 cat >> ~/.ssh/config << EOF
 Host github.com
 	IdentityFile ~/.ssh/gce_key
 	StrictHostKeyChecking no
 EOF
-
 
 # Move into scripts directory
 cd ..
@@ -60,12 +57,20 @@ cd "${SCRIPTS_DIR}"
 
 # Copy scripts into /usr/local/bin and make them executable
 for script in *; do
-    cp "${script}" /usr/local/bin
-    chmod +x "/usr/local/bin/${script}"
+    sudo cp "${script}" /usr/local/bin
+    sudo chmod +x "/usr/local/bin/${script}"
 done
 
 # Return to parent directory
 cd ..
 
 # Install crowdin-bot python package
-pip3 install "${PYTHON_PACKAGE_DIR}"/
+sudo pip3 install "${PYTHON_PACKAGE_DIR}"/
+
+# Setup crontab
+crontab << EOF
+SHELL=/bin/bash
+4 12 * * * PATH=\$PATH:/usr/local/bin; source crowdin-bot-env-secrets.sh; crowdin-bot-push-source.sh > crowdin-bot-push-source.log 2> crowdin-bot-push-source.err
+4 13 * * * PATH=\$PATH:/usr/local/bin; source crowdin-bot-env-secrets.sh; crowdin-bot-pull-translations.sh > crowdin-bot-push-source.log 2> crowdin-bot-pull-translations.err
+4 14 * * * PATH=\$PATH:/usr/local/bin; source crowdin-bot-env-secrets.sh; crowdin-bot-pull-incontext.sh > crowdin-bot-pull-incontext.log 2> crowdin-bot-pull-incontext.err
+EOF
