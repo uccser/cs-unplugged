dfc24cd3214b8b417882536bc96b1c94ff90dfbb
Jordan Griffiths <jordangriffiths01@gmail.com>
2017-11-22 15:07:07 +1300

    Implement unit tests for resource_parameters.py

--- csunplugged/tests/resources/utils/test_resource_parameters.py
+++ csunplugged/tests/resources/utils/test_resource_parameters.py
@@ -1,79 +1,227 @@
+from tests.BaseTest import BaseTest
+from lxml import etree
+from utils.errors.QueryParameterMissingError import QueryParameterMissingError
+from utils.errors.QueryParameterInvalidError import QueryParameterInvalidError
+from utils.errors.QueryParameterMultipleValuesError import QueryParameterMultipleValuesError
+
+from resources.utils.resource_parameters import (
+    ResourceParameter,
+    SingleValuedParameter,
+    MultiValuedParameter,
+    EnumResourceParameter,
+    BoolResourceParameter,
+    TextResourceParameter,
+    IntegerResourceParameter
+)
+
 class ResourceParametersTest(BaseTest):
 
     def test_resource_parameter_base_process_requested_values(self):
-        pass
+        param = ResourceParameter()
+        with self.assertRaises(NotImplementedError):
+            param.process_requested_values(None)
 
     def test_resource_parameter_base_process_value(self):
-        pass
+        param = ResourceParameter()
+        processed = param.process_value("value")
+        # Parameter should be unchanged
+        self.assertEqual("value", processed)
 
     def test_resource_parameter_base_html_element(self):
-        pass
+        param = ResourceParameter("param1", "param1 description")
+        html = param.html_element()
+        self.assertIsInstance(html, etree._Element)
+        self.assertEqual("fieldset", html.tag)
+        self.assertEqual(1, len(html)) # Should only have 1 child, the
+        self.assertEqual("legend", html[0].tag)
+        self.assertEqual("param1 description", html[0].text)
 
     def test_single_valued_parameter_process_requested_values_required_present(self):
-        pass
+        param = SingleValuedParameter()
+        param.process_requested_values([1])
+        self.assertEqual(1, param.value)
 
     def test_single_valued_parameter_process_requested_values_required_missing(self):
-        pass
+        param = SingleValuedParameter()
+        with self.assertRaises(QueryParameterMissingError):
+            param.process_requested_values([])
 
     def test_single_valued_parameter_process_requested_values_not_required_missing(self):
-        pass
+        param = SingleValuedParameter(default="default value", required=False)
+        param.process_requested_values([])
+        self.assertEqual("default value", param.value)
+
 
     def test_single_valued_parameter_process_requested_values_multiple_values(self):
-        pass
+        param = SingleValuedParameter()
+        with self.assertRaises(QueryParameterMultipleValuesError):
+            param.process_requested_values(["value1", "value2"])
 
     def test_multi_valued_parameter_process_requested_values_single_value(self):
-        pass
+        param = MultiValuedParameter()
+        param.process_requested_values(["value1"])
+        self.assertEqual(["value1"], param.values)
 
     def test_multi_valued_parameter_process_requested_values_multiple_values(self):
-        pass
+        param = MultiValuedParameter()
+        param.process_requested_values(["value1", "value2"])
+        self.assertEqual(["value1", "value2"], param.values)
 
     def test_enum_resource_parameter_html_element(self):
-        pass
+        values = {"value1": "Value 1", "value2": "Value 2"}
+        param = EnumResourceParameter(
+            name="option1",
+            values=values,
+            default="value2",
+            description="option1 description"
+        )
+        html = param.html_element()
+        self.assertIsInstance(html, etree._Element)
+        self.assertEqual("fieldset", html.tag)
+        self.assertEqual(7, len(html)) # legend, (input, label, br) X 2
 
-    def test_enum_resource_parameter_process_valid_value(self):
-        pass
+        self.assertEqual("legend", html[0].tag)
+        self.assertEqual("option1 description", html[0].text)
 
-    def test_enum_resource_parameter_process_invalid_value(self):
-        pass
+        self.assertEqual("input", html[1].tag)
+        self.assertEqual("option1", html[1].get("name"))
+        self.assertEqual("value1", html[1].get("value"))
+        self.assertEqual("radio", html[1].get("type"))
+        self.assertEqual(None, html[1].get("checked"))  # Not the default
 
-    def test_enum_resource_parameter_process_missing_value_with_default(self):
-        pass
+        self.assertEqual("label", html[2].tag)
+        self.assertEqual("Value 1", html[2].text)
 
-    def test_enum_resource_parameter_process_missing_value_without_default(self):
-        pass
+        self.assertEqual("br", html[3].tag)
+
+        self.assertEqual("input", html[4].tag)
+        self.assertEqual("option1", html[1].get("name"))
+        self.assertEqual("value2", html[4].get("value"))
+        self.assertEqual("radio", html[4].get("type"))
+        self.assertEqual("checked", html[4].get("checked"))  # default
+
+        self.assertEqual("label", html[5].tag)
+        self.assertEqual("Value 2", html[5].text)
+
+        self.assertEqual("br", html[6].tag)
+
+    def test_enum_resource_parameter_process_value_valid_value(self):
+        param = EnumResourceParameter(values={"value1": "Value 1"})
+        processed = param.process_value("value1")
+        self.assertEqual("value1", processed) # Returned unchanged
+
+    def test_enum_resource_parameter_process_value_invalid_value(self):
+        param = EnumResourceParameter(values={"value1": "Value 1"})
+        with self.assertRaises(QueryParameterInvalidError):
+            param.process_value("invalid")
 
     def test_enum_resource_parameter_index(self):
-        pass
+        param = EnumResourceParameter(values={"1":"One", "2":"Two", "3":"Three"})
+        self.assertEqual(1, param.index("2")) # Index of value 2 in the dict
 
     def test_bool_resource_parameter_default_text(self):
-        pass
+        param = BoolResourceParameter()
+        self.assertEqual({"yes": "Yes", "no": "No"}, param.valid_values)
 
     def test_bool_resource_parameter_custom_text(self):
-        pass
+        param = BoolResourceParameter(true_text="Custom1", false_text="Custom2")
+        self.assertEqual({"yes": "Custom1", "no": "Custom2"}, param.valid_values)
 
     def test_bool_resource_parameter_process_value_valid(self):
-        pass
+        param = BoolResourceParameter()
+        self.assertEqual(True, param.process_value("yes"))
+        self.assertEqual(False, param.process_value("no"))
 
     def test_bool_resource_parameter_process_value_invalid(self):
-        pass
+        param = BoolResourceParameter()
+        with self.assertRaises(QueryParameterInvalidError):
+            param.process_value("invalid")
 
     def test_text_resource_parameter_html_element(self):
-        pass
+        param = TextResourceParameter(name="option1")
+        html = param.html_element()
+        self.assertIsInstance(html, etree._Element)
+
+        self.assertEqual("fieldset", html.tag)
+        self.assertEqual(2, len(html)) # legend and input
+        self.assertEqual("legend", html[0].tag)
+
+        self.assertEqual("input", html[1].tag)
+        self.assertEqual("option1", html[1].get("name"))
+        self.assertEqual("", html[1].get("placeholder"))
+        self.assertEqual("text", html[1].get("type"))
 
     def test_text_resource_parameter_html_element_with_placeholder(self):
-        pass
+        param = TextResourceParameter(name="option1", placeholder="placeholder text")
+        html = param.html_element()
+        self.assertIsInstance(html, etree._Element)
+
+        self.assertEqual("fieldset", html.tag)
+        self.assertEqual(2, len(html)) # legend and input
+        self.assertEqual("legend", html[0].tag)
+
+        self.assertEqual("input", html[1].tag)
+        self.assertEqual("option1", html[1].get("name"))
+        self.assertEqual("placeholder text", html[1].get("placeholder"))
+        self.assertEqual("text", html[1].get("type"))
 
     def test_integer_resource_parameter_html_element(self):
-        pass
+        param = IntegerResourceParameter(name="option1")
+        html = param.html_element()
+        self.assertIsInstance(html, etree._Element)
+
+        self.assertEqual("fieldset", html.tag)
+        self.assertEqual(2, len(html)) # legend and input
+        self.assertEqual("legend", html[0].tag)
+
+        self.assertEqual("input", html[1].tag)
+        self.assertEqual("option1", html[1].get("name"))
+        self.assertEqual("number", html[1].get("type"))
+        self.assertEqual(None, html[1].get("min"))
+        self.assertEqual(None, html[1].get("max"))
+        self.assertEqual(None, html[1].get("value"))
 
     def test_integer_resource_parameter_html_element_with_min_max_default(self):
-        pass
+        param = IntegerResourceParameter(
+            name="option1",
+            default=20,
+            min_val=10,
+            max_val=30
+        )
+        html = param.html_element()
+        self.assertIsInstance(html, etree._Element)
 
-    def test_integer_resource_parameter_process_value(self):
-        pass
+        self.assertEqual("fieldset", html.tag)
+        self.assertEqual(2, len(html)) # legend and input
+        self.assertEqual("legend", html[0].tag)
 
-    def test_integer_resource_parameter_process_value_invalid_value_type(self):
-        pass
+        self.assertEqual("input", html[1].tag)
+        self.assertEqual("option1", html[1].get("name"))
+        self.assertEqual("number", html[1].get("type"))
+        self.assertEqual("10", html[1].get("min"))
+        self.assertEqual("30", html[1].get("max"))
+        self.assertEqual("20", html[1].get("value"))
+
+    def test_integer_resource_parameter_process_value_no_range(self):
+        param = IntegerResourceParameter(name="option1")
+        self.assertEqual(10, param.process_value("10"))
+        self.assertEqual(0, param.process_value("0"))
+        self.assertEqual(-10, param.process_value("-10"))
+
+    def test_integer_resource_parameter_process_value_in_range(self):
+        param = IntegerResourceParameter(name="option1", min_val=-20, max_val=20)
+        self.assertEqual(10, param.process_value("10"))
+        self.assertEqual(0, param.process_value("0"))
+        self.assertEqual(-10, param.process_value("-10"))
 
     def test_integer_resource_parameter_process_value_out_of_range(self):
-        pass
+        param = IntegerResourceParameter(name="option1", min_val=-20, max_val=20)
+        with self.assertRaises(QueryParameterInvalidError):
+            param.process_value("-30")
+        with self.assertRaises(QueryParameterInvalidError):
+            param.process_value("30")
+
+    def test_integer_resource_parameter_process_value_invalid_value_type(self):
+        param = IntegerResourceParameter(name="option1")
+        with self.assertRaises(QueryParameterInvalidError):
+            param.process_value("notaninteger")
