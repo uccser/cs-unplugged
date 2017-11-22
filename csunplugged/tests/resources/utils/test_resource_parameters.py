415a6eed4aeb53c0400326e5a6258ddb2ff39402
Jordan Griffiths <jordangriffiths01@gmail.com>
2017-11-22 13:50:56 +1300

    Add test skeleton for resource_parameters unit tests

---
+++ csunplugged/tests/resources/utils/test_resource_parameters.py
@@ -0,0 +1,79 @@
+class ResourceParametersTest(BaseTest):
+
+    def test_resource_parameter_base_process_requested_values(self):
+        pass
+
+    def test_resource_parameter_base_process_value(self):
+        pass
+
+    def test_resource_parameter_base_html_element(self):
+        pass
+
+    def test_single_valued_parameter_process_requested_values_required_present(self):
+        pass
+
+    def test_single_valued_parameter_process_requested_values_required_missing(self):
+        pass
+
+    def test_single_valued_parameter_process_requested_values_not_required_missing(self):
+        pass
+
+    def test_single_valued_parameter_process_requested_values_multiple_values(self):
+        pass
+
+    def test_multi_valued_parameter_process_requested_values_single_value(self):
+        pass
+
+    def test_multi_valued_parameter_process_requested_values_multiple_values(self):
+        pass
+
+    def test_enum_resource_parameter_html_element(self):
+        pass
+
+    def test_enum_resource_parameter_process_valid_value(self):
+        pass
+
+    def test_enum_resource_parameter_process_invalid_value(self):
+        pass
+
+    def test_enum_resource_parameter_process_missing_value_with_default(self):
+        pass
+
+    def test_enum_resource_parameter_process_missing_value_without_default(self):
+        pass
+
+    def test_enum_resource_parameter_index(self):
+        pass
+
+    def test_bool_resource_parameter_default_text(self):
+        pass
+
+    def test_bool_resource_parameter_custom_text(self):
+        pass
+
+    def test_bool_resource_parameter_process_value_valid(self):
+        pass
+
+    def test_bool_resource_parameter_process_value_invalid(self):
+        pass
+
+    def test_text_resource_parameter_html_element(self):
+        pass
+
+    def test_text_resource_parameter_html_element_with_placeholder(self):
+        pass
+
+    def test_integer_resource_parameter_html_element(self):
+        pass
+
+    def test_integer_resource_parameter_html_element_with_min_max_default(self):
+        pass
+
+    def test_integer_resource_parameter_process_value(self):
+        pass
+
+    def test_integer_resource_parameter_process_value_invalid_value_type(self):
+        pass
+
+    def test_integer_resource_parameter_process_value_out_of_range(self):
+        pass
