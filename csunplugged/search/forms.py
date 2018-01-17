23a08a375ff39365c2fdf3d533aa68ddb34d951e
Jack Morgan <jackmorgannz@gmail.com>
2018-01-17 20:14:42 +1300

    Pre-fill select with options from URL query

--- csunplugged/search/forms.py
+++ csunplugged/search/forms.py
@@ -1,7 +1,6 @@
 """Module for custom search form."""
 from django import forms
 from haystack.forms import ModelSearchForm
-from django_select2.forms import Select2MultipleWidget
 from django.db.models.functions import Concat
 from topics.models import (
     Lesson,
