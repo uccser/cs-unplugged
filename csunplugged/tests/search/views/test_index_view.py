85bae5695b6be6dab613bf9f86435050e67c0b1e
Jack Morgan <jackmorgannz@gmail.com>
2018-01-19 23:37:45 +1300

    Add tests for searching all items and type order

--- csunplugged/tests/search/views/test_index_view.py
+++ csunplugged/tests/search/views/test_index_view.py
@@ -357,3 +357,45 @@ class IndexViewTest(BaseTestWithDB):
                 "<CurriculumArea: Area 3>",
             ]
         )
+
+    def test_search_view_all_items(self):
+        topic = self.test_data.create_topic(1)
+        self.test_data.create_topic(2)
+        self.test_data.create_topic(3)
+        unit_plan = self.test_data.create_unit_plan(topic, 1)
+        unit_plan = self.test_data.create_unit_plan(topic, 2)
+        age_group = self.test_data.create_age_group(5, 7)
+        self.test_data.create_lesson(
+            topic,
+            unit_plan,
+            1,
+            age_group
+        )
+        management.call_command("rebuild_index", "--noinput")
+        url = reverse("search:index")
+        get_parameters = [("q", "")]
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        self.assertEqual(len(response.context["object_list"]), 6)
+
+    def test_search_view_assert_order(self):
+        topic = self.test_data.create_topic(1)
+        self.test_data.create_topic(2)
+        unit_plan = self.test_data.create_unit_plan(topic, 1)
+        age_group = self.test_data.create_age_group(5, 7)
+        self.test_data.create_lesson(
+            topic,
+            unit_plan,
+            1,
+            age_group
+        )
+        management.call_command("rebuild_index", "--noinput")
+        url = reverse("search:index")
+        get_parameters = [("q", "")]
+        url += query_string(get_parameters)
+        response = self.client.get(url)
+        result_objects = response.context["object_list"]
+        self.assertEqual(result_objects[0].model_name, "topic")
+        self.assertEqual(result_objects[1].model_name, "topic")
+        self.assertEqual(result_objects[2].model_name, "unitplan")
+        self.assertEqual(result_objects[3].model_name, "lesson")
