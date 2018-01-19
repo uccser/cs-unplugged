801227c8118ac535b027a5a18230e62c438a1e5a
Jack Morgan <jackmorgannz@gmail.com>
2018-01-19 15:01:14 +1300

    Override default method to return no items when there is no query
    This is not the default behaviour, and I am
    unsure of why it is being triggered. It would
    be wise to follow this up with the Django
    Haystack community.

--- csunplugged/search/views.py
+++ csunplugged/search/views.py
@@ -1,5 +1,6 @@
 """Module for custom search view."""
 from haystack.generic_views import SearchView
+from haystack.query import EmptySearchQuerySet
 from django.db.models.functions import Concat
 from search.forms import CustomSearchForm
 from topics.models import (
@@ -12,6 +13,20 @@ class CustomSearchView(SearchView):
     """View for custom search."""
 
     form_class = CustomSearchForm
+
+    def get_queryset(self):
+        """Return the list of items for this view.
+
+        This method overrides the default method, as all items were being
+        returned when no query parameters was given (not a blank query).
+
+        Returns:
+            QuerySet for view.
+        """
+        if self.request.GET:
+            return super(CustomSearchView, self).get_queryset()
+        else:
+            return EmptySearchQuerySet()
 
     def get_context_data(self, *args, **kwargs):
         """Return context dictionary for custom search view.
