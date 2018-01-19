ec4b6b7c29650ce094a0cb8d27f4495847b68118
Jack Morgan <jackmorgannz@gmail.com>
2018-01-20 00:06:33 +1300

    Add str, model_type, and absolute URL tests for indexed models

---
+++ csunplugged/tests/resources/models/test_resource.py
@@ -0,0 +1,39 @@
+from tests.BaseTestWithDB import BaseTestWithDB
+from tests.resources.ResourcesTestDataGenerator import ResourcesTestDataGenerator
+
+
+class ResourceModelTest(BaseTestWithDB):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.test_data = ResourcesTestDataGenerator()
+
+    def test_resource_str(self):
+        resource = self.test_data.create_resource(
+            "grid",
+            "Grid",
+            "resources/grid.html",
+            "GridResourceGenerator",
+        )
+        self.assertEqual(resource.__str__(), "Grid")
+
+    def test_resource_model_type(self):
+        resource = self.test_data.create_resource(
+            "grid",
+            "Grid",
+            "resources/grid.html",
+            "GridResourceGenerator",
+        )
+        self.assertEqual(resource.model_type(), "Resource")
+
+    def test_resource_model_get_absolute_url(self):
+        resource = self.test_data.create_resource(
+            "grid",
+            "Grid",
+            "resources/grid.html",
+            "GridResourceGenerator",
+        )
+        self.assertEqual(
+            resource.get_absolute_url(),
+            "/en/resources/grid/"
+        )
