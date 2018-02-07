26737c09481f2be9e224ec379751386cfa960e10
Sarang Love Leehan <saranglove.leehan@gmail.com>
2018-02-07 17:31:32 +1300

    Attempt to create left and right cards resource

--- csunplugged/resources/generators/LeftRightCardsResourceGenerator.py
+++ csunplugged/resources/generators/LeftRightCardsResourceGenerator.py
@@ -1,7 +1,9 @@
 """Class for Left and Right Cards resource generator."""
 
-from PIL import Image
+from PIL import Image, ImageDraw
 from resources.utils.BaseResourceGenerator import BaseResourceGenerator
+from django.utils.translation import ugettext as _
+from utils.TextBoxDrawer import TextBoxDrawer
 
 
 class LeftRightCardsResourceGenerator(BaseResourceGenerator):
@@ -13,6 +15,28 @@ class LeftRightCardsResourceGenerator(BaseResourceGenerator):
         Returns:
             A dictionary of the one page for the resource.
         """
-        image_path = "static/img/resources/left-right-cards/left-right-cards.png"
+        # image_path = "static/img/resources/left-right-cards/left-right-cards.png"
+        # image = Image.open(image_path)
+        # return {"type": "image", "data": image}
+
+        path = "static/img/resources/left-right-cards/left-right"
+        image_path = "{}.png".format(path)
+        svg_path = "{}.svg".format(path)
         image = Image.open(image_path)
+
+        draw = ImageDraw.Draw(image)
+        textbox_drawer = TextBoxDrawer(image, draw, svg_path)
+
+        for i in range(1, 5):
+            textbox_drawer.write_text_box(
+                "left{}".format(i),
+                _("LEFT"),
+                horiz_just="center"
+            )
+            textbox_drawer.write_text_box(
+                "right{}".format(i),
+                _("RIGHT"),
+                horiz_just="center"
+            )
+
         return {"type": "image", "data": image}
