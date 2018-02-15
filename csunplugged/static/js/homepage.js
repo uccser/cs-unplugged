0e034185f7eca0ae35c9b76d71f45cbb3d351b3e
Jack Morgan <jackmorgannz@gmail.com>
2018-02-15 15:47:02 +1300

    Only show transparent navbar on large devices

--- csunplugged/static/js/homepage.js
+++ csunplugged/static/js/homepage.js
@@ -1,6 +1,6 @@
+/* If within top 50px pixels of page, navbar becomes transparent */
 $(window).scroll(function() {
-  if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes transparent*/
-  {
+  if($(this).scrollTop() > 50) {
       $('.navbar').removeClass('trans-navbar');
   } else {
       $('.navbar').addClass('trans-navbar');
