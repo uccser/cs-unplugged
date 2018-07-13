47aa54829ec0a2f470103956fc349be046fea36d
Michael Colin Cowie <mco116@csse-s402g9.canterbury.ac.nz>
2018-07-13 15:09:24 +1200

    readded .po files. Minified codemirror css and js. Fixed js indentation problems

--- csunplugged/static/js/server-interpretation.js
+++ csunplugged/static/js/server-interpretation.js
@@ -1,30 +1,30 @@
-  var base_url = "http://36adab90.compilers.sphere-engine.com/api/v3/submissions/";
-  var token = "?access_token=5ec1d247f7d8b651411587faf1af080c";
+var base_url = "http://36adab90.compilers.sphere-engine.com/api/v3/submissions/";
+var token = "?access_token=5ec1d247f7d8b651411587faf1af080c";
 
-  function sendCode() {
-    $.ajax({
-      type: "POST",
-      url: base_url + token,
-      data: {
-        "language": 116,
-        "sourceCode": editor.getValue(),
-      },
-      async: true,
-      dataType: "json",
-      success: function(result, status){
-        id = result.id;
-        setTimeout(function(){
-          getCodeCompiled(id);
-        }, 1000);
-      },
-      error: function(result, status){
-        console.log("Error", result, status)
-      }
-    });
-  };
+function sendCode() {
+  $.ajax({
+    type: "POST",
+    url: base_url + token,
+    data: {
+      "language": 116,
+      "sourceCode": editor.getValue(),
+    },
+    async: true,
+    dataType: "json",
+    success: function(result, status){
+      id = result.id;
+      setTimeout(function(){
+        getCodeCompiled(id);
+      }, 1000);
+    },
+    error: function(result, status){
+      console.log("Error", result, status)
+    }
+  });
+};
 
 
- function getCodeCompiled(id) {
+function getCodeCompiled(id) {
   $.ajax({
     type: "GET",
     url: base_url + id + token,
