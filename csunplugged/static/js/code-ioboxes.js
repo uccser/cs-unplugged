cea17d9a828fef565644024f5eafe82b8c3aef72
Michael Colin Cowie <mco116@csse-s402g9.canterbury.ac.nz>
2018-08-05 15:56:34 +1200

    Working minimum example

--- csunplugged/static/js/code-ioboxes.js
+++ csunplugged/static/js/code-ioboxes.js
@@ -1,4 +1,4 @@
-var editor = CodeMirror(document.getElementById("codeeditor"), {
+var editor = CodeMirror(document.getElementById("outputField"), {
   lineNumbers : true,
   gutter: true,
   lineWrapping: true,
@@ -6,13 +6,13 @@ var editor = CodeMirror(document.getElementById("codeeditor"), {
   mode: "python"
 });
 
-var outputBox = CodeMirror(document.getElementById("outputBox"), {
+/*var outputBox = CodeMirror(document.getElementById("outputBox"), {
   gutter: true,
   lineWrapping: true,
   readOnly: false,
   cursorBlinkRate: -1,
   mode: "python"
-});
+});*/
 
 window.onload = function(){
   $('.CodeMirror-scroll').addClass('editor-height');
