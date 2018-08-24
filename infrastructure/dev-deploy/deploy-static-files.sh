96faedf6e8d9f8f62ab8d84c4ed0379ed1fa1973
Jack Morgan <jackmorgannz@gmail.com>
2018-08-24 15:54:11 +1200

    Modify static deploy script to call thumbnail command directly

--- infrastructure/dev-deploy/deploy-static-files.sh
+++ infrastructure/dev-deploy/deploy-static-files.sh
@@ -12,7 +12,7 @@ rm -r build/
 rm -r staticfiles/
 ./csu dev static_prod
 ./csu dev static_scratch
-./csu dev makeresourcethumbnails --all-languages
+docker-compose exec django /docker_venv/bin/python3 ./manage.py makeresourcethumbnails --all-languages
 ./csu dev collect_static
 
 # Install Google Cloud SDK
