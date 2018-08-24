37cb7a2b2b0d5b649fb94052728ffb91c7c5e71f
Jack Morgan <jackmorgannz@gmail.com>
2018-08-24 15:52:16 +1200

    (WIP) Add generation for map page of Treasure Island printable

---
+++ csunplugged/resources/content/en/treasure-island.md
@@ -0,0 +1,4 @@
+# Treasure Island
+
+This resource contains printouts for the Treasure Island activity for Finite State Automata.
+You will need a map printout for each participant, and one set of island posters.
--- csunplugged/resources/generators/TreasureIslandResourceGenerator.py
+++ csunplugged/resources/generators/TreasureIslandResourceGenerator.py
@@ -1,14 +1,91 @@
 """Class for Treasure Island resource generator."""
 
-from yattag import Doc
 from django.utils.translation import ugettext_lazy as _
-from django.utils.translation import get_language
+from PIL import Image, ImageDraw
+from utils.TextBoxDrawer import TextBoxDrawer, TextBox
 from resources.utils.BaseResourceGenerator import BaseResourceGenerator
 from resources.utils.resource_parameters import EnumResourceParameter
+from resources.utils.coords import calculate_box_vertices
 
+BASE_PATH = "static/img/resources/treasure-island/{}.png"
+MAP_TYPE = "map"
+ISLAND_TYPE = "island"
 TYPE_VALUES = {
-    "map": _("Student map"),
-    "islands": _("Islands"),
+    MAP_TYPE: _("Student map"),
+    # ISLAND_TYPE: _("Islands"),
+}
+FONT_PATH = "static/fonts/PatrickHand-Regular.ttf"
+FONT_COLOUR = "#000000"
+ISLANDS = {
+    "dead-mans-island": {
+        "name": _("Dead Man's Island"),
+        "map-location": {
+            "top-left-coords": (1206, 468),
+            "width": 732,
+            "height": 216,
+        },
+        "a": "musket-hill",
+        "b": "shipwreck-bay",
+    },
+    "musket-hill": {
+        "name": _("Musket Hill"),
+        "map-location": {
+            "top-left-coords": (537, 2052),
+            "width": 831,
+            "height": 141,
+        },
+        "a": "pirates-island",
+        "b": "mutineers-island",
+    },
+    "mutineers-island": {
+        "name": _("Mutineers' Island"),
+        "map-location": {
+            "top-left-coords": (1422, 1608),
+            "width": 690,
+            "height": 159,
+        },
+        "a": "smugglers-cove",
+        "b": "dead-mans-island",
+    },
+    "pirates-island": {
+        "name": _("Pirates' Island"),
+        "map-location": {
+            "top-left-coords": (72, 1362),
+            "width": 885,
+            "height": 204,
+        },
+        "a": "shipwreck-bay",
+        "b": "musket-hill",
+    },
+    "shipwreck-bay": {
+        "name": _("Shipwreck Bay"),
+        "map-location": {
+            "top-left-coords": (51, 546),
+            "width": 831,
+            "height": 225,
+        },
+        "a": "musket-hill",
+        "b": "dead-mans-island",
+    },
+    "smugglers-cove": {
+        "name": _("Smugglers' Cove"),
+        "map-location": {
+            "top-left-coords": (2298, 1209),
+            "width": 702,
+            "height": 204,
+        },
+        "a": "pirates-island",
+        "b": "treasure-island",
+    },
+    "treasure-island": {
+        "name": _("Treasure Island"),
+        "map-location": {
+            "top-left-coords": (2286, 444),
+            "width": 711,
+            "height": 237,
+        },
+        "goal": True,
+    },
 }
 
 
@@ -26,3 +103,59 @@ class TreasureIslandResourceGenerator(BaseResourceGenerator):
                 default="map"
             )
         }
+
+    def data(self):
+        """Create data for a copy of the Treasure Island resource.
+
+        Returns:
+            A dictionary of the data for the resource.
+        """
+        pages = []
+        if self.options["type"].value == MAP_TYPE:
+            path = BASE_PATH.format(MAP_TYPE)
+            image = Image.open(path)
+            draw = ImageDraw.Draw(image)
+            textbox_drawer = TextBoxDrawer(image, draw)
+
+            # Add island labels
+            for island_id, island_data in ISLANDS.items():
+                box_data = island_data["map-location"]
+                top_left_coords = box_data["top-left-coords"]
+                width = box_data["width"]
+                height = box_data["height"]
+                vertices = calculate_box_vertices(top_left_coords, width, height)
+                box = TextBox(
+                    vertices,
+                    width,
+                    height,
+                    font_path=FONT_PATH,
+                    font_size=10,
+                    color=FONT_COLOUR,
+                )
+                textbox_drawer.write_text_box(
+                    box,
+                    island_data["name"],
+                    horiz_just="center",
+                    vert_just="top",
+                )
+
+            image = image.rotate(90, expand=True)
+            pages.append({
+                "type": "image",
+                "data": image
+            })
+
+        return pages
+
+    @property
+    def subtitle(self):
+        """Return the subtitle string of the resource.
+
+        Used after the resource name in the filename, and
+        also on the resource image.
+
+        Returns:
+            text for subtitle (str).
+        """
+        printable_type = self.options["type"].value
+        return "{} - {}".format(TYPE_VALUES[printable_type], super().subtitle)
