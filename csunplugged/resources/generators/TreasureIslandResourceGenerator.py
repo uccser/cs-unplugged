92f6910cf0841273e4abc3c68b215ec727d6a622
Jack Morgan <jackmorgannz@gmail.com>
2018-08-26 22:15:26 +1200

    Complete logic for island posters on Treasure Island resource

--- csunplugged/resources/generators/TreasureIslandResourceGenerator.py
+++ csunplugged/resources/generators/TreasureIslandResourceGenerator.py
@@ -12,10 +12,14 @@ MAP_TYPE = "map"
 ISLAND_TYPE = "island"
 TYPE_VALUES = {
     MAP_TYPE: _("Student map"),
-    # ISLAND_TYPE: _("Islands"),
+    ISLAND_TYPE: _("Islands"),
 }
 FONT_PATH = "static/fonts/PatrickHand-Regular.ttf"
 FONT_COLOUR = "#000000"
+BOX_WIDTH = 700
+BOX_HEIGHT = int(BOX_WIDTH * 2 * 1.4 / 4)
+PAGE_WIDTH = BOX_WIDTH * 2
+PAGE_HEIGHT = BOX_HEIGHT * 4
 ISLANDS = {
     "dead-mans-island": {
         "name": _("Dead Man's Island"),
@@ -110,12 +114,18 @@ class TreasureIslandResourceGenerator(BaseResourceGenerator):
         Returns:
             A dictionary of the data for the resource.
         """
-        pages = []
         if self.options["type"].value == MAP_TYPE:
-            pages.append(self.create_map_page())
+            pages = self.create_map_page()
+        elif self.options["type"].value == ISLAND_TYPE:
+            pages = self.create_island_posters()
         return pages
 
     def create_map_page(self):
+        """Create map page for students.
+
+        Returns:
+            A dictionary of the data for the page.
+        """
         path = BASE_PATH.format(MAP_TYPE)
         image = Image.open(path)
         draw = ImageDraw.Draw(image)
@@ -148,6 +158,218 @@ class TreasureIslandResourceGenerator(BaseResourceGenerator):
             "data": image
         }
         return page_data
+
+    def create_island_posters(self):
+        """Create pages for island posters.
+
+        Layout of each island page (comprised of 5 boxes):
+        ┌─────────┐
+        │         │
+        │    A    │  A) Island
+        │         │  B) Boat A for island
+        ├────┬────┤  C) Boat B for island
+        │ B  │  C │  D) Destination for Boat A
+        ├────┼────┤  E) Destination for Boat B
+        │ D  │  E │
+        └────┴────┘
+
+        Returns:
+            List of dictionaries of data for island pages.
+        """
+        pages = []
+
+        for island_id, island_data in ISLANDS.items():
+            page = Image.new("RGB", (PAGE_WIDTH, PAGE_HEIGHT), "#fff")
+            draw = ImageDraw.Draw(page)
+
+            self.add_island_box(page, island_id, island_data)
+            if not island_data.get("goal", False):
+                # Section B
+                self.add_boat_box(page, "left", island_data["name"])
+                # Section C
+                self.add_boat_box(page, "right", island_data["name"])
+                # Section D
+                self.add_destination_box(page, "left", ISLANDS[island_data["a"]]["name"])
+                # Section E
+                self.add_destination_box(page, "right", ISLANDS[island_data["b"]]["name"])
+
+            # Draw grid lines
+            lines = [
+                # Top left to top right
+                ([(0, 0), (PAGE_WIDTH, 0)], 5, "#000"),
+                # Bottom left to bottom right
+                ([(0, PAGE_HEIGHT - 1), (PAGE_WIDTH, PAGE_HEIGHT - 1)], 5, "#000"),
+                # Top left to bottom left
+                ([(0, 0), (0, PAGE_HEIGHT)], 5, "#000"),
+                # Top right to bottom right
+                ([(PAGE_WIDTH - 1, 0), (PAGE_WIDTH - 1, PAGE_HEIGHT)], 5, "#000"),
+                # Mid left to mid right
+                ([(0, PAGE_HEIGHT / 2), (PAGE_WIDTH, PAGE_HEIGHT / 2)], 5, "#000"),
+                # Three quarter left to three quarter right
+                ([(0, PAGE_HEIGHT * 0.75), (PAGE_WIDTH, PAGE_HEIGHT * 0.75)], 1, "#555"),
+                # Center to bottom mid
+                ([(PAGE_WIDTH / 2, PAGE_HEIGHT / 2), (PAGE_WIDTH / 2, PAGE_HEIGHT - 1)], 5, "#000"),
+            ]
+            for line_coords, line_width, line_colour in lines:
+                # line_coords = [(int(x), int(y)) for (x, y) in line_coords]
+                draw.line(line_coords, fill=line_colour, width=line_width)
+            pages.append({"type": "image", "data": page})
+        pages[0]["thumbnail"] = True
+        return pages
+
+    def add_island_box(self, page, island_id, island_data):
+        """Add island label to top of page.
+
+        Args:
+            page (Image): Page to add image to.
+            island_id (str): ID of island.
+            island_data (dict): Data of island.
+        """
+        draw = ImageDraw.Draw(page)
+        textbox_drawer = TextBoxDrawer(page, draw)
+        island_image = Image.open(BASE_PATH.format(island_id))
+        island_width, island_height = island_image.size
+        coords = (int(BOX_WIDTH - (island_width / 2)), int(((BOX_HEIGHT * 2 * 0.7) - island_height) / 2))
+        page.paste(island_image, box=coords)
+        island_name_width = BOX_WIDTH * 2
+        island_name_height = BOX_HEIGHT * 2 * 0.3
+        vertices = calculate_box_vertices(
+            (0, BOX_HEIGHT * 2 * 0.7),
+            island_name_width,
+            island_name_height
+        )
+        box = TextBox(
+            vertices,
+            island_name_width,
+            island_name_height,
+            font_path=FONT_PATH,
+            font_size=200,
+            color=FONT_COLOUR,
+        )
+        textbox_drawer.write_text_box(
+            box,
+            island_data["name"],
+            horiz_just="center",
+            vert_just="top",
+        )
+
+    def add_boat_box(self, page, direction, island_name):
+        """Add boat boxes to page.
+
+        Args:
+            page (Image): Page to add image to.
+            direction (str): Direction boat is travelling.
+            island_name (str): Name of island.
+        """
+        box = Image.new("RGB", (BOX_WIDTH, BOX_HEIGHT), "#fff")
+        box_draw = ImageDraw.Draw(box)
+        box_textbox_drawer = TextBoxDrawer(box, box_draw)
+
+        # Add image
+        boat_image = Image.open(BASE_PATH.format("boat"))
+        boat_width, boat_height = boat_image.size
+        # Boxes are 70% boat image, 10% island name, 20% boat name
+        ratio = (BOX_HEIGHT * 0.7) / boat_height
+        boat_image = boat_image.resize(
+            (int(boat_width * ratio), int(boat_height * ratio))
+        )
+        if direction == "left":
+            boat_image = boat_image.transpose(Image.FLIP_LEFT_RIGHT)
+        boat_width, boat_height = boat_image.size
+
+        box.paste(
+            boat_image,
+            box=(int((BOX_WIDTH / 2) - (boat_width / 2)), 0)
+        )
+        # First Line Text
+        text_width = BOX_WIDTH
+        text_height = BOX_HEIGHT * 0.1
+        vertices = calculate_box_vertices(
+            (0, BOX_HEIGHT * 0.7),
+            text_width,
+            text_height,
+        )
+        text_box = TextBox(
+            vertices,
+            text_width,
+            text_height,
+            font_path=FONT_PATH,
+            font_size=50,
+            color=FONT_COLOUR,
+        )
+        box_textbox_drawer.write_text_box(
+            text_box,
+            island_name,
+            horiz_just="center",
+            vert_just="center",
+        )
+        # Second line of text
+        text_width = BOX_WIDTH
+        text_height = BOX_HEIGHT * 0.2
+        vertices = calculate_box_vertices(
+            (0, BOX_HEIGHT * 0.8),
+            text_width,
+            text_height,
+        )
+        text_box = TextBox(
+            vertices,
+            text_width,
+            text_height,
+            font_path=FONT_PATH,
+            font_size=70,
+            color=FONT_COLOUR,
+        )
+        if direction == "left":
+            boat_text = _("Boat A")
+            x_position = 0
+        else:
+            boat_text = _("Boat B")
+            x_position = BOX_WIDTH
+        box_textbox_drawer.write_text_box(
+            text_box,
+            boat_text,
+            horiz_just="center",
+            vert_just="center",
+        )
+        box = box.transpose(Image.ROTATE_180)
+        page.paste(
+            box,
+            box=(x_position, BOX_HEIGHT * 2)
+        )
+
+    def add_destination_box(self, page, direction, destination_name):
+        """Add destination boxes to page.
+
+        Args:
+            page (Image): Page to add image to.
+            direction (str): Direction boat is travelling.
+            destination_name (str): Name of destination island.
+        """
+        draw = ImageDraw.Draw(page)
+        textbox_drawer = TextBoxDrawer(page, draw)
+        if direction == "left":
+            x_position = 0
+        else:
+            x_position = BOX_WIDTH
+        vertices = calculate_box_vertices(
+            (x_position, BOX_HEIGHT * 3),
+            BOX_WIDTH,
+            BOX_HEIGHT,
+        )
+        text_box = TextBox(
+            vertices,
+            BOX_WIDTH,
+            BOX_HEIGHT,
+            font_path=FONT_PATH,
+            font_size=50,
+            color=FONT_COLOUR,
+        )
+        textbox_drawer.write_text_box(
+            text_box,
+            destination_name,
+            horiz_just="center",
+            vert_just="center",
+        )
 
     @property
     def subtitle(self):
