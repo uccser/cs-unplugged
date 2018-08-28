ce7224ea664a6810b57b00049403339dd66ecfef
Hayley van Waas <hayleyavw@gmail.com>
2018-08-28 18:09:26 +1200

    WIP rewriting the way the job badges printable is generated

--- csunplugged/resources/generators/JobBadgesResourceGenerator.py
+++ csunplugged/resources/generators/JobBadgesResourceGenerator.py
@@ -1,9 +1,36 @@
 """Class for Job Badges resource generator."""
 
 from PIL import Image, ImageDraw
-from resources.utils.BaseResourceGenerator import BaseResourceGenerator
+from utils.TextBoxDrawer import TextBox, TextBoxDrawer
 from django.utils.translation import ugettext_lazy as _
-from utils.TextBoxDrawer import TextBoxDrawer
+from resources.utils.BaseResourceGenerator import BaseResourceGenerator
+from resources.utils.coords import calculate_box_vertices
+
+FONT_PATH = "static/fonts/NotoSans-Regular.ttf"
+FONT_SIZE = 90
+LABEL_DATA = {
+    "programmer": {
+        "text": _("Hello, I'm a Programmer"),
+        "top-left-coords": (388, 4),
+        "width": 1435,
+        "height": 156,
+        "font-colour": "#000000",
+    },
+    "tester": {
+        "text": _("Hello, I'm a Tester"),
+        "top-left-coords": (388, 4),
+        "width": 1435,
+        "height": 156,
+        "horiz-just": "center",
+        "vert-just": "center",
+    },
+    "bot": {
+        "text": _("Hello, I'm a Bot"),
+        "top-left-coords": (388, 4),
+        "width": 1435,
+        "height": 156,
+    },
+}
 
 
 class JobBadgesResourceGenerator(BaseResourceGenerator):
@@ -15,13 +42,40 @@ class JobBadgesResourceGenerator(BaseResourceGenerator):
         Returns:
             A dictionary of the one page for the resource.
         """
-        path = "static/img/resources/job-badges/job-badges"
-        image_path = "{}.png".format(path)
-        svg_path = "{}.svg".format(path)
+        image_path = "static/img/resources/job-badges/job-badges.png"
         image = Image.open(image_path)
-
         draw = ImageDraw.Draw(image)
-        textbox_drawer = TextBoxDrawer(image, draw, svg_path)
+        textbox_drawer = TextBoxDrawer(image, draw)
+        
+        # Add text labels
+        for label_id, label_data in LABEL_DATA.items():
+            if label_id == "subtitle":
+                label_text = self.subtitle_text()
+            else:
+                label_text = label_data["text"]
+            top_left_coords = label_data["top-left-coords"]
+            width = label_data["width"]
+            height = label_data["height"]
+            vertices = calculate_box_vertices(top_left_coords, width, height)
+            box = TextBox(
+                vertices=vertices,
+                width=width,
+                height=height,
+                font_path=FONT_PATH,
+                font_size=FONT_SIZE,
+            )
+            textbox_drawer.write_text_box(
+                box,
+                label_text,
+                horiz_just="center",
+            )
+        '''
+        image_path = "static/img/resources/job-badges/job-badges.png"
+        image = Image.open(image_path)
+        draw = ImageDraw.Draw(image)
+
+        textbox_drawer = TextBoxDrawer(image, draw)
+
 
         hello_ids = [
             "programmer_hello1",
@@ -55,5 +109,6 @@ class JobBadgesResourceGenerator(BaseResourceGenerator):
                 _("Bot"),
                 horiz_just="center"
             )
+        '''
 
         return {"type": "image", "data": image}
