9396ebb0b42b0c5ee15c13955b8c7673aec57e4b
Oscar Stockill <ost18@uclive.ac.nz>
2019-06-26 14:51:00 +1200

    Hidden access key, hidden command executor and two local hosted server tests

--- csunplugged/tests/topics/in-browser/test_in_browser.py
+++ csunplugged/tests/topics/in-browser/test_in_browser.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 import time
+import os
+
+ACCESS_KEY = os.environ['KEY']
+COMMAND_EXECUTOR = 'http://cseducationresea1:' + ACCESS_KEY + '@hub.browserstack.com:80/wd/hub'
 
 v1 = {
     'os_version': '10',
@@ -42,71 +46,61 @@ local_server = {
 }
 
 
-def test_trial():
-    driver = webdriver.Remote(
-        command_executor='http://cseducationresea1:y1shw2Ck8GyVc53Y4LrT@hub.browserstack.com:80/wd/hub',
-        desired_capabilities=v1)
-
-    driver.get("https://csunplugged.org/en/")
-    if "CS Unplugged" not in driver.title:
-        raise Exception("Unable to load Cs unplugged page!")
-    # assert(driver.title.contains("CS Unplugged"))
-    driver.quit()
-
-
-def test_trial_15():
-    driver = webdriver.Remote(
-        command_executor='http://cseducationresea1:y1shw2Ck8GyVc53Y4LrT@hub.browserstack.com:80/wd/hub',
-        desired_capabilities=v2)
-
-    driver.get("https://csunplugged.org/en/")
-    if "CS Unplugged" not in driver.title:
-        raise Exception("Unable to load Cs unplugged page!")
-    print(driver.title)
-    driver.quit()
-
-
-def test_trial2():
-    driver = webdriver.Remote(
-        command_executor='http://cseducationresea1:y1shw2Ck8GyVc53Y4LrT@hub.browserstack.com:80/wd/hub',
-        desired_capabilities=v3)
-
-    driver.get("https://csunplugged.org/en/")
-    driver.find_element_by_xpath(
-        "(.//*[normalize-space(text()) and normalize-space(.)='Topics'])").click()
-    time.sleep(3)
-    if "Topics" not in driver.title:
-        raise Exception("Unable to load CS unplugged page!")
-    print(driver.title)
-    driver.quit()
+# def test_trial():
+#     driver = webdriver.Remote(
+#         command_executor=COMMAND_EXECUTOR,
+#         desired_capabilities=v1)
+#
+#     driver.get("https://csunplugged.org/en/")
+#     if "CS Unplugged" not in driver.title:
+#         raise Exception("Unable to load Cs unplugged page!")
+#     driver.quit()
+#
+#
+# def test_trial_15():
+#     driver = webdriver.Remote(
+#         command_executor=COMMAND_EXECUTOR,
+#         desired_capabilities=v2)
+#
+#     driver.get("https://csunplugged.org/en/")
+#     if "CS Unplugged" not in driver.title:
+#         raise Exception("Unable to load Cs unplugged page!")
+#     driver.quit()
+#
+#
+# def test_trial2():
+#     driver = webdriver.Remote(
+#         command_executor=COMMAND_EXECUTOR,
+#         desired_capabilities=v3)
+#
+#     driver.get("https://csunplugged.org/en/")
+#     driver.find_element_by_xpath(
+#         "(.//*[normalize-space(text()) and normalize-space(.)='Topics'])").click()
+#     time.sleep(3)
+#     if "Topics" not in driver.title:
+#         raise Exception("Unable to load CS unplugged page!")
+#     driver.quit()
 
 
 def test_local():
     driver = webdriver.Remote(
-        command_executor='http://cseducationresea1:y1shw2Ck8GyVc53Y4LrT@hub.browserstack.com:80/wd/hub',
+        command_executor=COMMAND_EXECUTOR,
         desired_capabilities=local_server)
 
-    driver.get("http://127.0.0.1:8000/polls/")
-    element = driver.find_element_by_tag_name("a")
-    if "what's up?" not in element.text:
+    driver.get("http://localhost/en")
+    element = driver.title
+    if "CS Unplugged" not in element.text:
         raise Exception("Unable to load local page!")
-    print(element.text)
     driver.quit()
 
 
-def test_browser_first():
+def test_local_2():
     driver = webdriver.Remote(
-        command_executor='http://cseducationresea1:y1shw2Ck8GyVc53Y4LrT@hub.browserstack.com:80/wd/hub',
+        command_executor=COMMAND_EXECUTOR,
         desired_capabilities=local_server)
 
-    driver.get("localhost/en/search/?q=")
-    driver.find_element_by_xpath(
-        "(.//*[normalize-space(text()) and normalize-space(.)='Show all curriculum areas'])[1]/following::div[1]").click()
-    driver.find_element_by_xpath(
-        "(.//*[normalize-space(text()) and normalize-space(.)='Logic'])[2]/following::input[1]").click()
-    element = driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Filter by curriculum areas'])[1]/following::span[1]")
-    print(element.text)
-    # if "what's up?" not in element.text:
-    #     raise Exception("Unable to load local page!")
-
+    driver.get("http://localhost/en/topics/searching-algorithms/")
+    element = driver.title
+    if "Searching Algorithms - CS Unplugged" not in element.text:
+        raise Exception("Unable to load local page!")
     driver.quit()
