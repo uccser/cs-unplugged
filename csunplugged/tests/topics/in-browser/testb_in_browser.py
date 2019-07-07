fb38ddc719a47654a14f5a501bc4cc97e4373ec2
Oscar Stockill <ost18@uclive.ac.nz>
2019-07-08 11:29:58 +1200

    Removed styling errors and attempting to click through site to see if this passes rather than loading topics page off the bat. This is perhaps not a good solution though as this will bloat the time of testing.

--- csunplugged/tests/topics/in-browser/testb_in_browser.py
+++ csunplugged/tests/topics/in-browser/testb_in_browser.py
@@ -96,13 +96,25 @@ class BrowserTest(TestCase):
             raise Exception("Unable to load local page!")
         driver.quit()
 
-
     def test_local_2(self):
         driver = webdriver.Remote(
             command_executor=COMMAND_EXECUTOR,
             desired_capabilities=local_server)
 
         driver.get("http://localhost/topics/")
+        element = driver.title
+        if "Topics - CS Unplugged" not in element:
+            raise Exception("Unable to load local page!")
+        driver.quit()
+
+    def test_local_3(self):
+        driver = webdriver.Remote(
+            command_executor=COMMAND_EXECUTOR,
+            desired_capabilities=local_server)
+
+        driver.get("http://localhost/")
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='How do I teach CS Unplugged?'])[1]/following::h2[1]").click()
         element = driver.title
         if "Topics - CS Unplugged" not in element:
             raise Exception("Unable to load local page!")
