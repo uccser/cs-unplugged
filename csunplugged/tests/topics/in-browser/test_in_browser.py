57cf6c629dfee125d1089a54296b07a7d284e690
Oscar Stockill <ost18@uclive.ac.nz>
2019-07-11 11:12:07 +1200

    Added selenium import to travis

--- csunplugged/tests/topics/in-browser/test_in_browser.py
+++ csunplugged/tests/topics/in-browser/test_in_browser.py
@@ -7,7 +7,6 @@ from django.test import TestCase, tag
 ACCESS_KEY = os.environ['KEY']
 COMMAND_EXECUTOR = 'http://cseducationresea1:' + ACCESS_KEY + '@hub.browserstack.com:80/wd/hub'
 
-print(os.environ)
 
 v1 = {
     'os_version': '10',
@@ -51,41 +50,6 @@ local_server = {
 }
 
 
-# def test_trial():
-#     driver = webdriver.Remote(
-#         command_executor=COMMAND_EXECUTOR,
-#         desired_capabilities=v1)
-#
-#     driver.get("https://csunplugged.org/en/")
-#     if "CS Unplugged" not in driver.title:
-#         raise Exception("Unable to load Cs unplugged page!")
-#     driver.quit()
-#
-#
-# def test_trial_15():
-#     driver = webdriver.Remote(
-#         command_executor=COMMAND_EXECUTOR,
-#         desired_capabilities=v2)
-#
-#     driver.get("https://csunplugged.org/en/")
-#     if "CS Unplugged" not in driver.title:
-#         raise Exception("Unable to load Cs unplugged page!")
-#     driver.quit()
-#
-#
-# def test_trial2():
-#     driver = webdriver.Remote(
-#         command_executor=COMMAND_EXECUTOR,
-#         desired_capabilities=v3)
-#
-#     driver.get("https://csunplugged.org/en/")
-#     driver.find_element_by_xpath(
-#         "(.//*[normalize-space(text()) and normalize-space(.)='Topics'])").click()
-#     time.sleep(3)
-#     if "Topics" not in driver.title:
-#         raise Exception("Unable to load CS unplugged page!")
-#     driver.quit()
-
 @tag("browser")
 class BrowserTest(unittest.TestCase):
     """ Test cases for the in-browser test suite"""
@@ -126,42 +90,3 @@ class BrowserTest(unittest.TestCase):
         if "CS Unplugged" not in element:
             raise Exception("Unable to load local page!")
         driver.quit()
-
-
-def test_local_2(self):
-    driver = webdriver.Remote(
-        command_executor=COMMAND_EXECUTOR,
-        desired_capabilities=local_server)
-
-    driver.get("http://localhost/en/resources/")
-    element = driver.title
-    if "Printables" not in element:
-        raise Exception("Unable to load local page!")
-    driver.quit()
-
-
-def test_local_3(self):
-    driver = webdriver.Remote(
-        command_executor=COMMAND_EXECUTOR,
-        desired_capabilities=local_server)
-
-    driver.get("http://localhost/")
-    driver.find_element_by_xpath(
-        "(.//*[normalize-space(text()) and normalize-space(.)="
-        "'How do I teach CS Unplugged?'])[1]/following::h2[1]").click()
-    element = driver.title
-    if "Topics - CS Unplugged" not in element:
-        raise Exception("Unable to load local page!")
-    driver.quit()
-
-
-def test_local():
-    driver = webdriver.Remote(
-        command_executor=COMMAND_EXECUTOR,
-        desired_capabilities=local_server)
-
-    driver.get("http://localhost/")
-    element = driver.title
-    if "CS Unplugged" not in element:
-        raise Exception("Unable to load local page!")
-    driver.quit()
