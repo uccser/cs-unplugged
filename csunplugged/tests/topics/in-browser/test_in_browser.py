fe0bd3c0ec2a7adddf0a727d243c26f231142ef4
Oscar Stockill <ost18@uclive.ac.nz>
2019-07-11 13:01:25 +1200

    Running tests alongside existing two that accesses the resources page

--- csunplugged/tests/topics/in-browser/test_in_browser.py
+++ csunplugged/tests/topics/in-browser/test_in_browser.py
@@ -54,17 +54,16 @@ local_server = {
 class BrowserTest(unittest.TestCase):
     """ Test cases for the in-browser test suite"""
 
-    # def test_local_2_test(self):
-    #     driver = webdriver.Remote(
-    #         command_executor=COMMAND_EXECUTOR,
-    #         desired_capabilities=local_server)
-    #
-    #     driver.get("http://localhost/en/resources/")
-    #     element = driver.title
-    #     assert "Printables" in element
-    #     if "Printables" not in element:
-    #         raise Exception("Unable to load local page!")
-    #     driver.quit()
+    def test_local_2_test(self):
+        driver = webdriver.Remote(
+            command_executor=COMMAND_EXECUTOR,
+            desired_capabilities=local_server)
+
+        driver.get("http://localhost/en/resources/")
+        element = driver.title
+        if "Printables" not in element:
+            raise Exception("Unable to load local page!")
+        driver.quit()
     #
     # def test_local_3(self):
     #     driver = webdriver.Remote(
