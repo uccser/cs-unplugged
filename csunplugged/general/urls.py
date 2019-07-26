a94a723bfd211c1c960b1e9ae3cf308644584f56
courtneycb <courtneybracefield86@gmail.com>
2019-07-26 15:49:36 +1200

    Style

--- csunplugged/general/urls.py
+++ csunplugged/general/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 from . import views
 
-app_name="general"
+app_name = "general"
 urlpatterns = [
     path(
         "",
--- csunplugged/search/urls.py
+++ csunplugged/search/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 from . import views
 
-app_name="search"
+app_name = "search"
 urlpatterns = [
     # eg: /search/
     path(
