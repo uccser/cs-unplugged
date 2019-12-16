7858ff2222d91a55c8a1c3b3f9b8bf082bb0d1ac
JackMorganNZ <jackmorgannz@gmail.com>
2019-12-16 22:16:07 +1300

    Fix bug with order of URL paths
    Also adds trailing slashes.

--- csunplugged/topics/urls.py
+++ csunplugged/topics/urls.py
@@ -48,6 +48,18 @@ urlpatterns = [
         views.OtherResourcesView.as_view(),
         name="other_resources"
     ),
+    # eg: /topics/binary-numbers/programming/challenge-1/
+    url(
+        r"^(?P<topic_slug>[-\w]+)/programming/(?P<programming_challenge_slug>[-\w]+)/$",  # noqa: E501
+        views.ProgrammingChallengeView.as_view(),
+        name="programming_challenge"
+    ),
+    # eg: /topics/binary-numbers/programming/challenge-1/python-solution/
+    url(
+        r"^(?P<topic_slug>[-\w]+)/programming/(?P<programming_challenge_slug>[-\w]+)/(?P<programming_language_slug>[-\w]+)-solution/$",  # noqa: E501
+        views.ProgrammingChallengeLanguageSolutionView.as_view(),
+        name="programming_challenge_solution"
+    ),
     # eg: /topics/binary-numbers/unit-plan/
     url(
         r"^(?P<topic_slug>[-\w]+)/(?P<unit_plan_slug>[-\w]+)/$",
@@ -71,17 +83,5 @@ urlpatterns = [
         r"^(?P<topic_slug>[-\w]+)/(?P<unit_plan_slug>[-\w]+)/(?P<lesson_slug>[-\w]+)/programming/$",
         views.ProgrammingChallengeList.as_view(),
         name="programming_challenges_list"
-    ),
-    # eg: /topics/binary-numbers/programming/challenge-1/
-    url(
-        r"^(?P<topic_slug>[-\w]+)/programming/(?P<programming_challenge_slug>[-\w]+)$",  # noqa: E501
-        views.ProgrammingChallengeView.as_view(),
-        name="programming_challenge"
-    ),
-    # eg: /topics/binary-numbers/programming/challenge-1/python-solution/
-    url(
-        r"^(?P<topic_slug>[-\w]+)/programming/(?P<programming_challenge_slug>[-\w]+)/(?P<programming_language_slug>[-\w]+)-solution$",  # noqa: E501
-        views.ProgrammingChallengeLanguageSolutionView.as_view(),
-        name="programming_challenge_solution"
     ),
 ]
