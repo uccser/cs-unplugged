a26871a0630ba436fb417d1e4572e3b3e0093bd7
Dana Lambert <dla72@uclive.ac.nz>
2020-03-31 16:47:36 +1300

    Adding in loader for test-cases from files

--- csunplugged/topics/management/commands/_ProgrammingChallengesLoader.py
+++ csunplugged/topics/management/commands/_ProgrammingChallengesLoader.py
@@ -6,6 +6,9 @@ from utils.errors.KeyNotFoundError import KeyNotFoundError
 from utils.errors.MissingRequiredFieldError import MissingRequiredFieldError
 from utils.TranslatableModelLoader import TranslatableModelLoader
 
+from pluggingitin.models import (
+    TestCase
+)
 
 from topics.models import (
     LearningOutcome,
@@ -168,13 +171,47 @@ class ProgrammingChallengesLoader(TranslatableModelLoader):
                 LOG_TEMPLATE = "Added language implementation: {}"
                 self.log(LOG_TEMPLATE.format(implementation.language), 2)
 
+            test_cases = challenge_structure.get("test-cases", None)
+            if (test_cases is not None):
+                for (testcase_id, testcase_type) in test_cases.items():
+                    test_case_translations = self.get_blank_translation_dictionary()
+
+                    testcase_filename_template = os.path.join(
+                        challenge_slug,
+                        'test-cases',
+                        "test-case-{}-{{}}.txt".format(testcase_id)
+                    )
+
+                    testcase_input = open(self.get_localised_file(
+                            "en", testcase_filename_template.format(testcase_type)), encoding='UTF-8').read()
+                    
+                    testcase_output = open(self.get_localised_file(
+                            "en", testcase_filename_template.format("output")), encoding='UTF-8').read()
+
+                    test_case=TestCase(
+                        number = testcase_id,
+                        test_input = testcase_input,
+                        expected_output = testcase_output,
+                        question_type = testcase_type,
+                        challenge = programming_challenge
+                    )
+
+                    required_fields=['test_input', 'expected_output', 'question_type']
+
+                    self.populate_translations(test_case, test_case_translations)
+                    self.mark_translation_availability(test_case, required_fields = required_fields)
+                    test_case.save()
+
+                    LOG_TEMPLATE="Added Programming Challenge Test Case: {}"
+                    self.log(LOG_TEMPLATE.format(testcase_id), 2)
+
             if "learning-outcomes" in challenge_structure:
-                learning_outcomes = challenge_structure["learning-outcomes"]
+                learning_outcomes=challenge_structure["learning-outcomes"]
                 if learning_outcomes is not None:
                     for learning_outcome_slug in learning_outcomes:
                         try:
-                            learning_outcome = LearningOutcome.objects.get(
-                                slug=learning_outcome_slug
+                            learning_outcome=LearningOutcome.objects.get(
+                                slug = learning_outcome_slug
                             )
                             programming_challenge.learning_outcomes.add(learning_outcome)
                         except ObjectDoesNotExist:
