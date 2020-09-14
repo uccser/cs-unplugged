ac8e013738fe68d62274be1dc462ceec560d737d
Dana Lambert <dla72@uclive.ac.nz>
2020-09-14 22:10:21 +1200

    Add url unit tests for plugging it in

---
+++ csunplugged/tests/plugging_it_in/urls/test_programming_challenge.py
@@ -0,0 +1,18 @@
+from tests.BaseTestWithDB import BaseTestWithDB
+from django.urls import reverse
+
+
+class ProgrammingChallengeURLTest(BaseTestWithDB):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.language = "en"
+
+    def test_valid_programming_challenge(self):
+        kwargs = {
+            "topic_slug": "binary-numbers",
+            "lesson_slug": "lesson-1",
+            "programming_challenge_slug": "challenge-1",
+        }
+        url = reverse("plugging_it_in:programming_challenge", kwargs=kwargs)
+        self.assertEqual(url, "/en/plugging-it-in/binary-numbers/lesson-1/challenge-1/")
