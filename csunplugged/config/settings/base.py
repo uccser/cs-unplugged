8607175ba42b1a9354b042ab56977ee2df674122
Jack Morgan <jackmorgannz@gmail.com>
2021-07-13 16:47:55 +1200

    Use whitenoise to serve static files

--- csunplugged/config/settings/base.py
+++ csunplugged/config/settings/base.py
@@ -71,6 +71,7 @@ INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 # ----------------------------------------------------------------------------
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
