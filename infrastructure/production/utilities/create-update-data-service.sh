842162420a842ef77596292db5a7c525b8c38303
Jack Morgan <jackmorgannz@gmail.com>
2021-07-29 15:25:48 +1200

    Add script for creating update data docker service

---
+++ infrastructure/production/utilities/create-update-data-service.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+docker service create \
+    --name cs-unplugged_update-data \
+    --detach \
+    --mode replicated-job \
+    --label traefik.enable=false \
+    --network cs-unplugged_backend \
+    --constraint node.role==worker \
+    --constraint node.labels.role==apps \
+    --env POSTGRES_HOST="postgres" \
+    --env=POSTGRES_PORT="5432" \
+    --env=DJANGO_SECRET_KEY_FILE="/run/secrets/cs-unplugged_django_secret_key" \
+    --env=POSTGRES_DB_FILE="/run/secrets/cs-unplugged_postgres_db" \
+    --env=POSTGRES_USER_FILE="/run/secrets/cs-unplugged_postgres_user" \
+    --env=POSTGRES_PASSWORD_FILE="/run/secrets/cs-unplugged_postgres_password" \
+    --secret cs-unplugged_django_secret_key \
+    --secret cs-unplugged_postgres_db \
+    --secret cs-unplugged_postgres_user \
+    --secret cs-unplugged_postgres_password \
+    --restart-condition none \
+    ghcr.io/uccser/cs-unplugged:develop python ./manage.py updatedata
