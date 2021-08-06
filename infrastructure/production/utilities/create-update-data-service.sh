ce4a8acd26c1a4cae60aef89049f5f6122f3e4ff
Jack Morgan <jackmorgannz@gmail.com>
2021-08-06 14:34:41 +1200

    Include config with Docker job

--- infrastructure/production/utilities/create-update-data-service.sh
+++ infrastructure/production/utilities/create-update-data-service.sh
@@ -10,10 +10,12 @@ docker service create \
     --constraint node.labels.role==apps \
     --env POSTGRES_HOST="postgres" \
     --env=POSTGRES_PORT="5432" \
+    --env=DEPLOYMENT_ENVIRONMENT_FILE="/cs-field-guide_deployment_environment" \
     --env=DJANGO_SECRET_KEY_FILE="/run/secrets/cs-unplugged_django_secret_key" \
     --env=POSTGRES_DB_FILE="/run/secrets/cs-unplugged_postgres_db" \
     --env=POSTGRES_USER_FILE="/run/secrets/cs-unplugged_postgres_user" \
     --env=POSTGRES_PASSWORD_FILE="/run/secrets/cs-unplugged_postgres_password" \
+    --config cs-field-guide_deployment_environment \
     --secret cs-unplugged_django_secret_key \
     --secret cs-unplugged_postgres_db \
     --secret cs-unplugged_postgres_user \
