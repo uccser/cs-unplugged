66763beab4a189de3b5caa1af5844cb4150cf7f3
Rchi Lugtu <rcl51@uclive.ac.nz>
2021-08-13 12:03:31 +1200

    Added a new language called ‘Block-based’ for one of the questions in Kidbots. Block-based language is now displayed as one of the programming language options in Kidbots. Also added information panel when the tab is on Block-based. Changed the URL to display /python or /block-based at the end when user clicks on a programming challenge.

--- csunplugged/plugging_it_in/urls.py
+++ csunplugged/plugging_it_in/urls.py
@@ -28,6 +28,11 @@ urlpatterns = [
         name="programming_challenge"
     ),
     url(
+         r"^(?P<topic_slug>[-\w]+)/(?P<lesson_slug>[-\w]+)/(?P<programming_challenge_slug>[-\w]+)/(?P<programming_lang_slug>[-\w]+)/$",
+         views.ProgrammingChallengeView.as_view(),
+         name="programming_challenge"
+    ),
+    url(
         r"^jobe_proxy$",
         views.JobeProxyView.as_view(),
         name="jobe_proxy"
---
+++ csunplugged/topics/content/en/kidbots/programming-challenges/modulo-even-odd-numbers/block-based-expected.md
@@ -0,0 +1,15 @@
+```scratch
+when green flag clicked
+
+ask [Enter a number:] and wait
+
+set [number v] to (answer)
+
+say [You entered an even number!]
+
+say [You entered an odd number!]
+
+if <((number) mod (2)) = [0]> then
+else
+end
+```
