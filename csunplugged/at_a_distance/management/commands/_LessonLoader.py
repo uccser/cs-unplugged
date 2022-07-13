ad953493242935dc83583f8f8075f890b59f14bf
JackMorganNZ <jackmorgannz@gmail.com>
2022-07-13 14:06:47 +1200

    Store supporting resources in database

--- csunplugged/at_a_distance/management/commands/_LessonLoader.py
+++ csunplugged/at_a_distance/management/commands/_LessonLoader.py
@@ -2,9 +2,17 @@
 
 from django.db import transaction
 from utils.TranslatableModelLoader import TranslatableModelLoader
+from utils.errors.CouldNotFindYAMLFileError import CouldNotFindYAMLFileError
 from utils.errors.MissingRequiredFieldError import MissingRequiredFieldError
-from at_a_distance.models import Lesson
-from at_a_distance.settings import AT_A_DISTANCE_INTRODUCTION_FILENAME
+from utils.language_utils import (
+    get_available_languages,
+    get_default_language,
+)
+from at_a_distance.models import Lesson, SupportingResource
+from at_a_distance.settings import (
+    AT_A_DISTANCE_INTRODUCTION_FILENAME,
+    AT_A_DISTANCE_SUPPORTING_RESOURCES_FILENAME,
+)
 
 
 class AtADistanceLessonLoader(TranslatableModelLoader):
@@ -59,8 +67,44 @@ class AtADistanceLessonLoader(TranslatableModelLoader):
         self.mark_translation_availability(lesson, required_fields=['name', 'introduction'])
         lesson.save()
 
+        # Supporting resources
+        lesson.supporting_resources.all().delete()
+        supporting_resources = self.lesson_data.get('supporting-resources')
+        if supporting_resources:
+            self.add_supporting_resource_translations(lesson)
+
         if created:
             term = 'Created'
         else:
             term = 'Updated'
         self.log(f'{term} At A Distance Lesson: {lesson}')
+
+    def add_supporting_resource_translations(self, lesson):
+        """Get dictionary of translations of supporting resources.
+
+        Returns:
+            Dictionary mapping language codes to HTML.
+
+        Raises:
+            CouldNotFindYAMLFileError: If the requested file could not be found
+                in the /en directory tree
+        """
+        for language in get_available_languages():
+            yaml_file_path = self.get_localised_file(
+                language,
+                AT_A_DISTANCE_SUPPORTING_RESOURCES_FILENAME,
+            )
+            try:
+                supporting_resources = self.load_yaml_file(yaml_file_path)
+            except CouldNotFindYAMLFileError:
+                if language == get_default_language():
+                    raise
+            else:
+                for (index, supporting_resource) in enumerate(supporting_resources):
+                    SupportingResource.objects.create(
+                        order_number=index,
+                        text=supporting_resource['text'],
+                        url=supporting_resource['url'],
+                        language=language,
+                        lesson=lesson,
+                    )
--- csunplugged/at_a_distance/models.py
+++ csunplugged/at_a_distance/models.py
@@ -3,9 +3,18 @@
 from os.path import join
 from django.urls import reverse
 from django.db import models
+from django.utils.translation import get_language
 from django.utils.translation import ugettext_lazy as _
 from utils.TranslatableModel import TranslatableModel
 from at_a_distance.settings import AT_A_DISTANCE_SLIDES_TEMPLATE_BASE_PATH
+
+
+class LessonManager(models.Manager):
+    """Custom manager for Lesson class."""
+
+    def get_queryset(self):
+        """Prefetch related supporting resources."""
+        return super().get_queryset().prefetch_related('supporting_resources')
 
 
 class Lesson(TranslatableModel):
@@ -17,6 +26,7 @@ class Lesson(TranslatableModel):
     name = models.CharField(max_length=200, default="")
     order_number = models.PositiveSmallIntegerField(unique=True)
     icon = models.CharField(max_length=150, null=True)
+    objects = LessonManager()
     # Content
     introduction = models.TextField(default="")
     video = models.URLField(blank=True)
@@ -46,5 +56,38 @@ class Lesson(TranslatableModel):
 
     class Meta:
         """Set consistent ordering of lessons."""
+
+        ordering = ["order_number", ]
+
+
+class SupportingResourceManager(models.Manager):
+    """Custom manager for SupportingResource class."""
+
+    def get_queryset(self):
+        """Return supporting resources for the current language."""
+        return super().get_queryset().filter(language=get_language())
+
+
+class SupportingResource(models.Model):
+    """Model for a supporting resource to a lesson.
+
+    This model is not translatable as supporting resources may be
+    completely different for each language.
+    """
+
+    order_number = models.PositiveSmallIntegerField()
+    text = models.TextField()
+    url = models.URLField()
+    language = models.CharField(max_length=10)
+    lesson = models.ForeignKey(
+        Lesson,
+        null=True,
+        related_name="supporting_resources",
+        on_delete=models.CASCADE,
+    )
+    objects = SupportingResourceManager()
+
+    class Meta:
+        """Set consistent ordering of supporting resources."""
 
         ordering = ["order_number", ]
