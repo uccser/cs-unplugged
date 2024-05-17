05b35537560079a7cb49c38414ac7e4494b3d96c
jhn27 <jamesnapier64@gmail.com>
2024-05-17 15:21:38 +1200

    Fix Sorting Network Cards resource for Pillow 10

--- csunplugged/resources/generators/SortingNetworkCardsResourceGenerator.py
+++ csunplugged/resources/generators/SortingNetworkCardsResourceGenerator.py
@@ -169,9 +169,10 @@ class SortingNetworkCardsResourceGenerator(BaseResourceGenerator):
                 else:
                     (x, y) = card_centers[1]
 
-                text_width, text_height = draw.textsize(text_string, font=font)
+                left, top, right, bottom = draw.textbbox((0, 0), text_string, font=font)
+                text_width, text_height = right - left, bottom - top
                 coord_x = x - (text_width / 2)
-                coord_y = y - (text_height / 1.5)
+                coord_y = y - text_height
                 draw.text(
                     (coord_x, coord_y),
                     text_string,
